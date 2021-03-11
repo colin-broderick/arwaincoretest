90ee5e29d6ced2d8730fa14476af760e041022ae
Colin Broderick <colin.broderick@greeve.co.uk>
2021-03-11 14:43:42 +0000

    Still on LoRa, some transmissions successully received

--- lora_driver/lora_tx_test.cpp
+++ lora_driver/lora_tx_test.cpp
@@ -16,14 +16,15 @@ int main()
     }
     lora.setFrequency(LoRa::FREQ_868)
         ->setTXPower(17)
-        ->setSpreadFactor(LoRa::SF_7)
-        ->setBandwidth(LoRa::BW_500k)
-        ->setCodingRate(LoRa::CR_45)
+        ->setSpreadFactor(LoRa::SF_12)
+        ->setBandwidth(LoRa::BW_125k)
+        ->setCodingRate(LoRa::CR_48)
         ->setSyncWord(0x12)
-        ->setHeaderMode(LoRa::HM_IMPLICIT)
+        ->setHeaderMode(LoRa::HM_EXPLICIT)
         ->enableCRC();
     
-    static const char *message = "Hello";
+    static const char *message = "eHellop";
     LoRaPacket p((unsigned char*)message, strlen(message));
     size_t bytes = lora.transmitPacket(&p);
+    std::cout << "Packet sent" << std::endl;
 }
