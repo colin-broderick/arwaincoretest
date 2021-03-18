3fba5b4b6bc15749838ebad966d1ae957dd42eff
ldisafjlhsdaflksdj <sdlifohujsdlkfghsdflikh@dkjsafslkdjfgl>
2021-03-18 14:47:55 +0000

    Added isnan safety catch to madgwick filter in case of bad sensor reading, continued development of standalone madgwick

--- orientation/filter.h
+++ orientation/filter.h
@@ -15,10 +15,10 @@ namespace arwain
             virtual ~Filter() = default;
             virtual void update(double timestamp, double gx, double gy, double gz, double ax, double ay, double az) = 0;
             virtual void update(double timestamp, double gx, double gy, double gz, double ax, double ay, double az, double mx, double my, double mz) = 0;
-            virtual double getW() = 0;
-            virtual double getX() = 0;
-            virtual double getY() = 0;
-            virtual double getZ() = 0;
+            virtual double getW() {};
+            virtual double getX() {};
+            virtual double getY() {};
+            virtual double getZ() {};
             virtual double getPitch() = 0;
             virtual double getRoll() = 0;
             virtual double getYaw() = 0;
