83dd2c9844878b63217ff9f57f0b8c12d290e5bb
Colin Broderick <colin.broderick@greeve.co.uk>
2021-03-24 13:56:36 +0000

    Added slow inference with interpolation, added path drawing script

--- orientation/filter.h
+++ orientation/filter.h
@@ -15,10 +15,10 @@ namespace arwain
             virtual ~Filter() = default;
             virtual void update(double timestamp, double gx, double gy, double gz, double ax, double ay, double az) = 0;
             virtual void update(double timestamp, double gx, double gy, double gz, double ax, double ay, double az, double mx, double my, double mz) = 0;
-            virtual double getW() {return 0;}
-            virtual double getX() {return 0;}
-            virtual double getY() {return 0;}
-            virtual double getZ() {return 0;}
+            virtual double getW() = 0;
+            virtual double getX() = 0;
+            virtual double getY() = 0;
+            virtual double getZ() = 0;
             virtual double getPitch() = 0;
             virtual double getRoll() = 0;
             virtual double getYaw() = 0;
