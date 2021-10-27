f5b8cca8ace35afc53c914fc69f1b31def445d35
Colin Broderick <colin.broderick@greeve.co.uk>
2021-10-27 16:14:51 +0100

    Replaced quaternion library with (a better) one of my own. Lots of features missing from the old one.

--- quaternion/quaternion.hpp
+++ quaternion/quaternion.hpp
@@ -1,68 +1,90 @@
-/* Copyright (c) 2012 Alex Allen, MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining w copy of this software 
- * and associated documentation files (the "Software"), to deal in the Software without restriction, 
- * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or 
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
- * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-#ifndef QUAT_H
-#define QUAT_H
+#ifndef _ARWAIN_QUATERNIONS_HPP
+#define _ARWAIN_QUATERNIONS_HPP
 
 #include <iostream>
+#include <cmath>
+#include <array>
+
+// Class layout ===================================================================================
 
 class quaternion
 {
+    private:
+        double angle;
+        std::array<double, 3> axis;
+
     public:
-        //  q = w + ix + jy + kz
         double w, x, y, z;
 
-        // Constructors.
+        // Constructors
         quaternion();
-        quaternion(double real, double im_i, double im_j, double im_k);
-        quaternion(const double x, const double y, const double z);
-        
-        // Operators.
-        quaternion operator+(quaternion quat2);
-        quaternion operator-(quaternion quat2);
-        quaternion operator*(quaternion quat2);
-        quaternion operator*(double num);
-        quaternion operator/(double num);
-        quaternion operator+=(quaternion quat2);
-        quaternion operator-=(quaternion quat2);
-        quaternion operator*=(quaternion quat2);
-        quaternion operator*=(double num);
-        quaternion operator/=(double num);
-        int operator==(quaternion q2);
-        
-        // General methods.
-        double mag(); // magnitude
-        quaternion unit(); // returns unit quaternion
-        quaternion conj(); // returns the conjugate
-        quaternion inv(); // returns the inverse
-        quaternion unit_inv(); // inverse for unit quaternion
-        
-        // Getters.
-        double getRe(); // return real component
-        double getIm_i(); // return i component
-        double getIm_j(); // return j component
-        double getIm_k(); // return k component
+        quaternion(const std::array<double, 3>& vec);
+        quaternion(const double angle, const std::array<double, 3>& axis);
+        quaternion(const double w, const double x, const double y, const double z);
+
+        // Static methods
+        static double dotProduct(const quaternion& quat1, const quaternion& quat2);
+        static quaternion slerp(const quaternion& quat1, const quaternion& quat2);
+
+        // Getters
+        double getW() const;
+        double getX() const;
+        double getY() const;
+        double getZ() const;
+        double getAngle() const;
+        std::array<double, 3> getAxis() const;
+        double norm() const;
+        bool isNormal() const;
+        quaternion unit() const;
+        quaternion normalized() const;
+        quaternion conjugate() const;
+        quaternion inverse() const;
 };
 
-inline std::ostream& operator<<(std::ostream& stream, quaternion quat)
+bool operator==(const quaternion& quat1, const quaternion& quat2);
+quaternion operator+(const quaternion& quat1, const quaternion& quat2);
+quaternion operator-(const quaternion& quat1, const quaternion& quat2);
+quaternion operator*(const quaternion& quat1, const quaternion& quat2);
+quaternion operator-(const quaternion& quat);
+bool operator==(const quaternion& quat1, const quaternion& quat2);
+std::ostream& operator<<(std::ostream& stream, const quaternion& quat);
+
+/** \brief Multiply a quaternion by a scalar value.
+ * \param scalar The scalar value by which to multiply.
+ * \param quat The quaternion to multiply.
+ * \return The new quaternion after multiplication.
+ */
+template<typename Scalar>
+quaternion operator*(const Scalar scalar, const quaternion& quat)
 {
-    stream << "quaternion(" << quat.w << ", " << quat.x << ", " << quat.y << ", " << quat.z << ")";
-    return stream;
+    return quaternion{
+        scalar * quat.getW(),
+        scalar * quat.getX(),
+        scalar * quat.getY(),
+        scalar * quat.getZ()
+    };
 }
- 
+
+/** \brief Multiply a quaternion by a scalar value.
+ * \param quaternion The quaternion to multiply.
+ * \param scalar The scalar value by which to multiply.
+ * \return The new quaternion after multiplication.
+ */
+template<typename Scalar>
+quaternion operator*(const quaternion& quat, const Scalar scalar)
+{
+    return scalar * quat;
+}
+
+/** \brief Divide a quaternion by a scalar value, i.e. multiply that that values inverse.
+ * \param quaternion The quaternion to divide.
+ * \param divisor The scalar value by which to divide.
+ * \return The new quaternion after division.
+ */
+template<typename Divisor>
+quaternion operator/(const quaternion& quat, const Divisor divisor)
+{
+    return (1.0/divisor) * quat;
+}
+
 #endif
