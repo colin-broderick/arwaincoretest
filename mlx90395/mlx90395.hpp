5d6a25e4dbea5a5934d66f17b8486ab58f214b83
Colin Broderick <colin.broderick@greeve.co.uk>
2021-11-09 10:20:18 +0000

    MLX driver now as complete as I can realistically make it without hardware to test

--- mlx90395/mlx90395.hpp
+++ mlx90395/mlx90395.hpp
@@ -39,13 +39,19 @@ extern "C"
 #define TMP_CMP_DS 0b00000000_00000000
 
 #define CMD_RESET 0xF0
+#define CMD_BURST 0x1E
+#define CMD_BURST_WITH_T 0x1F
+#define CMD_READ 0x40
 #define CMD_EXIT 0x80
 
 // DATA REGISTERS ================================================================
-#define CONFIGURATION_01 0x01
-#define CONFIGURATION_02 0x02
+#define REGISTER_0 0x00
+#define REGISTER_1 0x02
+#define REGISTER_2 0x04
+
 #define CMD_TARGET 0x80
 #define READ_TARGET 0x80
+#define STATUS_RESET 0x02
 
 // DATA CONVERSION FACTORS =========================================================
 
@@ -57,15 +63,36 @@ public:
     quaternion read_orientation();
     vector3 calibrate();
 
+    enum OSR
+    {
+        OSR_1 = 0,
+        OSR_2,
+        OSR_4,
+        OSR_8
+    };
+
+    enum Resolution
+    {
+        RES_16 = 0,
+        RES_17,
+        RES_18,
+        RES_19
+    };
+
 private:
     bool configure();
     bool reset();
     bool exit_mode();
+    uint8_t burst_mode();
     void send_read_command();
+    bool set_data_rate(uint8_t rate);
+    uint8_t command(uint8_t cmd);
     uint8_t get_gain();
     uint8_t get_resolution();
-    void set_osr(uint8_t);
-    void set_resolution(uint8_t);
+    void set_gain(uint8_t gain_);
+
+    void set_osr(OSR osr_val);
+    void set_resolution(Resolution res);
     void i2c_init(const int address, const std::string &bus_name);
     int i2c_read(int reg_addr, int bytes, uint8_t *buffer);
     int i2c_write(int reg_addr, int bytes, uint8_t *buffer);
@@ -76,9 +103,7 @@ private:
     double mag_gain;       // todo check data type
     double mag_resolution; // todo check data type
     double uTLSB;          // todo check data type
-    const std::array<float, 16> gain_multipliers = {
-        0.2, 0.25, 0.3333, 0.4, 0.5, 0.6, 0.75, 1,
-        0.1, 0.125, 0.1667, 0.2, 0.25, 0.3, 0.375, 0.5};
+    
 };
 
 #endif
