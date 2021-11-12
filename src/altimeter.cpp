a89de44461e2d8d26d31965c864174ba85fafc28
Colin Broderick <colin.broderick@greeve.co.uk>
2021-11-12 11:01:58 +0000

    Alitimeter now uses BMP384 instead of BMP280

--- src/altimeter.cpp
+++ src/altimeter.cpp
@@ -1,9 +1,10 @@
 #include <mutex>
 
 #include "bmp280.cpp"
+#include "bmp384.hpp"
 #include "arwain.hpp"
 
-/** \brief Uses the BMP280 pressure sensor to determine altitude. */
+/** \brief Uses the BMP384 pressure sensor to determine altitude. */
 void altimeter()
 {
     // Quit immediately if pressure sensor disabled by configuration.
@@ -12,12 +13,7 @@ void altimeter()
         return;
     }
 
-    // Initialize the sensor.
-    bmp280_dev bmp;
-    bmp280_config conf;
-    bmp280_uncomp_data uncomp_data;
-    double pres, temp, alt;
-    init_bmp280(bmp, conf, uncomp_data, arwain::config.sea_level_pressure);
+    BMP384 bmp384{arwain::config.pressure_address, arwain::config.pressure_bus};
 
     // Set up timing.
     auto loopTime = std::chrono::system_clock::now();
@@ -26,22 +22,14 @@ void altimeter()
     // Spin until shutdown signal received.
     while (!arwain::shutdown)
     {
-        // Read the raw data
-        bmp280_get_uncomp_data(&uncomp_data, &bmp);
-
-        // Convert the raw data to doubles and calcualte altitude.
-        bmp280_get_comp_pres_double(&pres, uncomp_data.uncomp_press, &bmp);
-        bmp280_get_comp_temp_double(&temp, uncomp_data.uncomp_temp, &bmp);
-        alt = altitude_from_pressure_and_temperature(pres / 100.0, temp);
-
-        // Store output
+        auto [pressure, temperature] = bmp384.read();
+        double alt = BMP384::calculate_altitude(pressure / 100.0, temperature, arwain::config.sea_level_pressure);
         {
             std::lock_guard<std::mutex> lock{arwain::Locks::PRESSURE_BUFFER_LOCK};
             arwain::Buffers::PRESSURE_BUFFER.pop_front();
-            arwain::Buffers::PRESSURE_BUFFER.push_back({pres / 100.0, temp, alt});
+            arwain::Buffers::PRESSURE_BUFFER.push_back({pressure / 100.0, temperature, alt});
         }
-
-        // Wait until next scheduled run.
+        // Wait until next tick.
         loopTime = loopTime + interval;
         std::this_thread::sleep_until(loopTime);
     }
