7e97bfd47618434983ced981634c73827d61276c
Colin Broderick <colin.broderick@greeve.co.uk>
2021-11-15 11:23:37 +0000

    Added command line arg and func to test lora chip

--- src/main.cpp
+++ src/main.cpp
@@ -89,6 +89,11 @@ int main(int argc, char **argv)
         ret = arwain::test_imu();
     }
 
+    else if (input.contains("-testlora"))
+    {
+        ret = arwain::test_lora();
+    }
+
     else if (input.contains("-testori"))
     {
         int rate;
