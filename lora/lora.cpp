00c3b659dffd76c9299a999751d5b982b8950da2
Colin Broderick <colin.broderick@greeve.co.uk>
2021-11-15 11:23:10 +0000

    Added new LoRa driver

--- lora/lora.cpp
+++ lora/lora.cpp
@@ -1,385 +1,161 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <memory>
-#include <unistd.h>
-#include <wiringPi.h>
-#include <wiringPiSPI.h>
+#include <string>
+#include <stdexcept>
+#include <iostream>
 
-#include "lora.h"
-#include "packet.h"
+#include "lora.hpp"
 
-const uint32_t LoRa::bw[10] = {
-	7800,  10400,  15600,
-	20800, 31250,  41700,
-	62500, 125000, 250000,
-	500000
-};
-
-LoRa::LoRa(uint8_t spi, uint8_t ss, uint8_t dio0, uint8_t rst) : _spi_channel(spi),
-																 _ss_pin(ss),
-																 _dio0_pin(dio0),
-																 _rst_pin(rst) {}
-
-LoRa *LoRa::setSpreadFactor(LoRa::sf_t sf)
+LoRa::LoRa(const std::string &address)
 {
-	sleep();
-	sf = sf < SF_6
-			 ? SF_6
-			 : sf > SF_12
-				   ? SF_12
-				   : sf;
-	if (sf == SF_6)
-	{
-		setHeaderMode(HM_IMPLICIT);
-		writeRegister(REG_DETECT_OPTIMIZE, 0x05);
-		writeRegister(REG_DETECT_THRESH, 0x0C);
-	}
-	else
-	{
-		writeRegister(REG_DETECT_OPTIMIZE, 0x03);
-		writeRegister(REG_DETECT_THRESH, 0x0A);
-	}
-	writeRegister(REG_MODEM_CONFIG2, (readRegister(REG_MODEM_CONFIG2) & 0x0F) | (sf << 4));
-	return this;
+    // Create SPI device
+    spi_config = spi_config_t{0, 8, 1000000, 0};
+    spi = new SPI(address.c_str(), &spi_config);
+    if (!spi->begin())
+    {
+        throw std::runtime_error("Error starting SPI device - incorrecct device path?");
+    };
+    this->configure(868, 0);
 }
 
-LoRa::sf_t LoRa::getSpreadFactor()
+LoRa::~LoRa()
 {
-	return (LoRa::sf_t)(readRegister(REG_MODEM_CONFIG2) >> 4);
+    delete spi;
 }
 
-LoRa *LoRa::setFrequency(uint32_t freq)
+/** \brief Sets radio frequency, bandwidth, spread factor, LNA, PA. */
+void LoRa::configure(const int frequency_mhz, const bool rx_only)
 {
-	uint64_t frf = ((uint64_t)freq << 19) / OSC_FREQ;
-	writeRegister(REG_FRF_MSB, (uint8_t)(frf >> 16));
-	writeRegister(REG_FRF_MID, (uint8_t)(frf >> 8));
-	writeRegister(REG_FRF_LSB, (uint8_t)(frf >> 0));
-	if (freq < FREQ_868)
-	{
-		// set LowFrequencyMode
-		writeRegister(REG_OPMODE, readRegister(REG_OPMODE & 0xC7) | 0x08);
-	}
-	return this;
+    uint8_t op_mode = OPMODE_LONGRANGE | OPMODE_SLEEP;
+
+    // Set radio frequency.
+    uint32_t FRF_val;
+    switch (frequency_mhz)
+    {
+    case 433:
+        FRF_val = 7109363;
+        op_mode |= OPMODE_LOWFREQMODEON;
+        break;
+    case 868:
+        FRF_val = 14221348;
+        break;
+    case 915:
+        FRF_val = 14991398;
+        break;
+    case 923:
+        FRF_val = 15122470;
+        break;
+    }
+    uint8_t FRFMSB = ((FRF_val & 0x00FF0000) >> 16);
+    int8_t FRFMIB = ((FRF_val & 0x0000FF00) >> 8);
+    uint8_t FRFLSB = (FRF_val & 0x000000FF);
+    this->write_register(OPMODE_ADDRESS, op_mode);
+    this->write_register(FRFMSB_ADDRESS, FRFMSB);
+    this->write_register(FRFMIB_ADDRESS, FRFMIB);
+    this->write_register(FRFLSB_ADDRESS, FRFLSB);
+
+    // Set bandwidth to 500 KHz.
+    uint8_t modem_conf = this->read_register(MODEMCONFIG1_ADDRESS);
+    modem_conf &= ~(MODEMCONFIG1_BW_7_8K | MODEMCONFIG1_BW_10_4K | MODEMCONFIG1_BW_15_6K |
+                    MODEMCONFIG1_BW_20_8K | MODEMCONFIG1_BW_31_25K | MODEMCONFIG1_BW_41_7K |
+                    MODEMCONFIG1_BW_62_5K | MODEMCONFIG1_BW_125K | MODEMCONFIG1_BW_250K | MODEMCONFIG1_BW_500K);
+    modem_conf |= MODEMCONFIG1_BW_500K;
+    modem_conf &= ~MODEMCONFIG1_IMPLHDR;
+    this->write_register(MODEMCONFIG1_ADDRESS, modem_conf);
+
+    // Set spread factor to 12.
+    modem_conf = this->read_register(MODEMCONFIG2_ADDRESS);
+    modem_conf &= ~(MODEMCONFIG2_SF(6) | MODEMCONFIG2_SF(7) | MODEMCONFIG2_SF(8) | MODEMCONFIG2_SF(9) | MODEMCONFIG2_SF(10) | MODEMCONFIG2_SF(11) | MODEMCONFIG2_SF(12));
+    modem_conf |= MODEMCONFIG2_SF(12);
+    this->write_register(MODEMCONFIG2_ADDRESS, modem_conf);
+
+    if (rx_only)
+    {
+        //goto continuous RX
+        op_mode &= ~OPMODE_SLEEP;
+        op_mode |= OPMODE_RXCONTINUOUS;
+    }
+    else
+    {
+        //goto standby mode
+        op_mode &= ~OPMODE_SLEEP;
+        op_mode |= OPMODE_STDBY;
+    }
+
+    // Configure power amplifier.
+    uint8_t PA_config = this->read_register(PACONFIG_ADDRESS);
+    PA_config |= PACONFIG_PABOOST;
+    PA_config |= PACONFIG_MAXPWR(7);
+    PA_config |= PACONFIG_OUTPWR(0xf);
+    this->write_register(PACONFIG_ADDRESS, PA_config);
+
+    // Enable low noise amplified for high frequencies.
+    uint8_t LNA = LNA_GAIN_G1;
+    if (frequency_mhz > 525)
+    {
+        LNA |= LNA_BOOST_HF;
+    }
+    this->write_register(LNA_ADDRESS, LNA);
+
+    this->write_register(OPMODE_ADDRESS, op_mode);
+    this->write_register(IRQFLAGS_ADDRESS, 0xff); //clear flags
 }
 
-uint32_t LoRa::getFrequency()
+/** \brief Writes a message to the FIFO. Does not transmit. */
+void LoRa::write_FIFO(const char *str, uint8_t num_bytes)
 {
-	uint64_t frf = (readRegister(REG_FRF_MSB) << 16 |
-					readRegister(REG_FRF_MID) << 8 |
-					readRegister(REG_FRF_LSB) << 0);
-	LoRa::freq_t freq = (LoRa::freq_t)((uint64_t)(OSC_FREQ * frf) >> 19);
-	return freq;
+    const int max_message_size = 63;
+    uint8_t bf[max_message_size + 1] = {0};
+    bf[0] = FIFO_ADDRESS | 0x80;
+    for (int i = 0; i < num_bytes && i < max_message_size; i++)
+    {
+        bf[i + 1] = str[i];
+    }
+    this->spi->write(bf, num_bytes + 1);
 }
 
-LoRa *LoRa::setBandwidth(bw_t bandwidth)
+/** \brief Transmits arbitrary message encoded as a string. */
+void LoRa::send_message(const std::string &message)
 {
-	writeRegister(REG_MODEM_CONFIG1, (readRegister(REG_MODEM_CONFIG1) & 0x0F) | (bandwidth << 4));
-	long symDuration = 1000 / (bw[getBandwidth()] / (1L << getSpreadFactor()));
-	// Set LowDataRateOptimize if symbol duration > 16ms
-	writeRegister(REG_MODEM_CONFIG3, (readRegister(REG_MODEM_CONFIG3) & 0x07) | (symDuration > 16 ? 0x08 : 0x00));
-	return this;
+    uint8_t FIFOTxBase = this->read_register(FIFOTXBASE_ADDRESS); //get FIFO write position
+
+    this->write_register(FIFOPTR_ADDRESS, FIFOTxBase); //set FIFO pointer to write position
+    this->write_register(PAYLOADLEN_ADDRESS, message.size()); //payload is this long
+    this->write_FIFO(message.c_str(), message.size()); //write data into FIFO
+    this->write_register(IRQFLAGS_ADDRESS, 0xff); //clear flags
+
+    // enter TX mode
+    uint8_t op_mode = this->read_register(OPMODE_ADDRESS);
+    op_mode &= ~(OPMODE_CAD | OPMODE_RXSINGLE | OPMODE_RXCONTINUOUS | OPMODE_FSRX | OPMODE_FSTX | OPMODE_STDBY | OPMODE_SLEEP);
+    op_mode |= OPMODE_TX | OPMODE_LONGRANGE;
+    this->write_register(OPMODE_ADDRESS, op_mode);
+
+    // Wait until the TX flag is set on.
+    uint8_t IRQFlag;
+    do
+    {
+        IRQFlag = this->read_register(IRQFLAGS_ADDRESS);
+    } while ((IRQFlag & IRQ_TXDONE) == 0);
+
+    // Clear TX flag.
+    this->write_register(IRQFLAGS_ADDRESS, IRQ_TXDONE);
 }
 
-LoRa::bw_t LoRa::getBandwidth()
+/** \brief Reads a register which should contain a known value to ensure the chip is detected.
+ * Should return 0x1A.
+*/
+uint8_t LoRa::test_chip()
 {
-	return (LoRa::bw_t)(readRegister(REG_MODEM_CONFIG1) >> 4);
+    return read_register(0x02);
 }
 
-LoRa *LoRa::setTXPower(uint8_t p)
+uint8_t LoRa::read_register(uint8_t address)
 {
-	p = p < 2
-			? 2
-			: p > 17
-				  ? 17
-				  : p;
-	writeRegister(REG_PA_CONFIG, (uint8_t)(PA_BOOST | (p - 2)));
-	return this;
+    uint8_t bf[2] = {address & 0x7F, 0x00};
+    spi->xfer(bf, 2, bf, 2);
+    return bf[1];
 }
 
-uint8_t LoRa::getTXPower()
+void LoRa::write_register(uint8_t address, uint8_t value)
 {
-	uint8_t pa_config = readRegister(REG_PA_CONFIG);
-	double pmax = pa_config & PA_BOOST
-					  ? 17.0
-					  : 10.8 + 0.6 * (pa_config & 0x38);
-	return pmax - (15 - (pa_config & 0x0F));
-}
-
-LoRa *LoRa::setHeaderMode(hm_t hm)
-{
-	writeRegister(REG_MODEM_CONFIG1, (readRegister(REG_MODEM_CONFIG1) & 0xFE) | hm);
-	return this;
-}
-
-LoRa::hm_t LoRa::getHeaderMode()
-{
-	return (LoRa::hm_t)(readRegister(REG_MODEM_CONFIG1) & 0x01);
-}
-
-LoRa *LoRa::setCodingRate(cr_t cr)
-{
-	writeRegister(REG_MODEM_CONFIG1, (readRegister(REG_MODEM_CONFIG1) & 0xF1) | (cr << 1));
-	return this;
-}
-
-LoRa::cr_t LoRa::getCodingRate()
-{
-	return (LoRa::cr_t)((readRegister(REG_MODEM_CONFIG1) & 0x0E) >> 1);
-}
-
-LoRa *LoRa::setSyncWord(uint8_t sw)
-{
-	writeRegister(REG_SYNC_WORD, sw);
-	return this;
-}
-
-uint8_t LoRa::getSyncWord()
-{
-	return readRegister(REG_SYNC_WORD);
-}
-
-LoRa *LoRa::enableCRC()
-{
-	writeRegister(REG_MODEM_CONFIG2, readRegister(REG_MODEM_CONFIG2) | 0x04);
-	return this;
-}
-
-LoRa *LoRa::disableCRC()
-{
-	writeRegister(REG_MODEM_CONFIG2, readRegister(REG_MODEM_CONFIG2) & 0xFB);
-	return this;
-}
-
-LoRa *LoRa::setLNAGain(lna_gain_t gain)
-{
-	if (gain == LNA_AGC)
-	{
-		writeRegister(REG_MODEM_CONFIG3, readRegister(REG_MODEM_CONFIG3) | 0x04);
-	}
-	else
-	{
-		writeRegister(REG_MODEM_CONFIG3, readRegister(REG_MODEM_CONFIG3) & 0xFB);
-		writeRegister(REG_LNA, (readRegister(REG_LNA) & 0x1F) | gain);
-	}
-	return this;
-}
-
-LoRa::lna_gain_t LoRa::getLNAGain()
-{
-	if (readRegister(REG_MODEM_CONFIG3) & 0x04)
-	{
-		return LNA_AGC;
-	}
-	return (LoRa::lna_gain_t)(readRegister(REG_LNA) >> 5);
-}
-
-LoRa *LoRa::setLNABoost(bool boost)
-{
-	writeRegister(REG_LNA, (readRegister(REG_LNA) & 0xFA) | (boost ? 0x03 : 0x00));
-	return this;
-}
-
-bool LoRa::getLNABoost()
-{
-	return (readRegister(REG_LNA) & 0x03) == 0x03;
-}
-
-int LoRa::getFrequencyError()
-{
-	uint32_t fei =
-		(readRegister(REG_FEI_MSB) & 0x0F) << 16 |
-		readRegister(REG_FEI_MID) << 8 |
-		readRegister(REG_FEI_LSB);
-	return (uint32_t)((double)((uint64_t)(fei << 24) / OSC_FREQ) * (double)(bw[getBandwidth()] / 500.0));
-}
-
-void LoRa::selectReceiver() { digitalWrite(_ss_pin, LOW); }
-void LoRa::deselectReceiver() { digitalWrite(_ss_pin, HIGH); }
-
-uint8_t LoRa::readRegister(uint8_t addr)
-{
-	_spibuf[0] = addr & 0x7f;
-	_spibuf[1] = 0x00;
-	selectReceiver();
-	wiringPiSPIDataRW(_spi_channel, _spibuf, 2);
-	deselectReceiver();
-	return _spibuf[1];
-}
-
-void LoRa::writeRegister(uint8_t addr, uint8_t val)
-{
-	_spibuf[0] = addr | 0x80;
-	_spibuf[1] = val;
-	selectReceiver();
-	wiringPiSPIDataRW(_spi_channel, _spibuf, 2);
-	deselectReceiver();
-}
-
-void LoRa::setOpMode(uint8_t mode)
-{
-	// Preserve AccessSharedReg and LowFrequencyModeOn
-	writeRegister(REG_OPMODE, (readRegister(REG_OPMODE) & 0xC8) | OPMODE_LORA | mode);
-}
-
-uint8_t LoRa::getOpMode()
-{
-	return readRegister(REG_OPMODE) & OPMODE_MASK;
-}
-
-void LoRa::sleep()
-{
-	if (getOpMode() != OPMODE_SLEEP)
-	{
-		setOpMode(OPMODE_SLEEP);
-	}
-}
-
-void LoRa::standby()
-{
-	if (getOpMode() != OPMODE_STANDBY)
-	{
-		setOpMode(OPMODE_STANDBY);
-	}
-}
-
-uint8_t LoRa::version()
-{
-	digitalWrite(_rst_pin, LOW);
-	delay(100);
-	digitalWrite(_rst_pin, HIGH);
-	delay(100);
-	uint8_t version = readRegister(REG_VERSION);
-	return version;
-}
-
-bool LoRa::begin()
-{
-	wiringPiSetup();
-	pinMode(_ss_pin, OUTPUT);
-	pinMode(_dio0_pin, INPUT);
-	pinMode(_rst_pin, OUTPUT);
-	wiringPiSPISetup(_spi_channel, 500000);
-	sleep();
-	if (version() != 0x12)
-	{
-		// Unknown chipset
-		return false;
-	}
-	writeRegister(REG_FIFO_TX_BASE_ADDR, 0);
-	writeRegister(REG_FIFO_RX_BASE_ADDR, 0);
-	setLNABoost(true);
-	setLNAGain(LNA_AGC);
-	writeRegister(REG_PA_RAMP, (readRegister(REG_PA_RAMP) & 0xF0) | 0x08);
-	return true;
-}
-
-size_t LoRa::receive(unsigned char *buf)
-{
-	size_t bytes = 0;
-	uint8_t irq_flags = readRegister(REG_IRQ_FLAGS);
-	writeRegister(REG_IRQ_FLAGS, irq_flags); // clear IRQs
-	if ((irq_flags & IRQ_RXDONE_MASK) && ((irq_flags & IRQ_CRCERR_MASK) == 0)) {
-		// Received a packet
-		bytes = readRegister(REG_RX_NB_BYTES);
-		writeRegister(REG_FIFO_ADDR_PTR, readRegister(REG_FIFO_RX_CURRENT_ADDR));
-		setOpMode(OPMODE_STANDBY);
-		for (size_t i = 0; i < bytes; i++) {
-			*(buf + i) = readRegister(REG_FIFO);
-		}
-	}
-	return bytes;
-}
-
-LoRaPacket LoRa::receivePacket()
-{
-	/*
-	TODO
-
-	In order to enable a received loop we would want a receive method with a timeout, so we can
-	switch back to transmit mode when the schedule demands it. This method would be the logical
-	place to implement that timeout.
-
-	Replace the while loop with
-
-		while ((bytes = receive(buf)) == 0 || time < switch_time);
-
-	where switch_time is the time we want to receive until. This obviously makes it possible to
-	fail to receive messages, so messages will need repeated transmissions for safety. For exa-
-	mple, sea level pressure updates could be transmitted every minute or so.
-	*/
-
-	unsigned char buf[256];
-	setHeaderMode(HM_EXPLICIT);
-	writeRegister(REG_FIFO_ADDR_PTR, 0);
-	setOpMode(OPMODE_RX);
-	size_t bytes = 0;
-	while ((bytes = receive(buf)) == 0) {  //   <--- timeout here
-		usleep(10000);
-	}
-	int packet_rssi = readRegister(REG_PACKET_RSSI) - (getFrequency() < FREQ_868 ? 164 : 157);
-	float snr = ((int8_t)readRegister(REG_PACKET_SNR)) * 0.25;
-	return LoRaPacket(buf, bytes, packet_rssi, getFrequencyError(), snr);
-}
-
-size_t LoRa::write(const uint8_t *data, size_t size)
-{
-	uint8_t currentLength = readRegister(REG_PAYLOAD_LENGTH);
-	if (currentLength + size > MAX_PACKET_LEN)
-	{
-		size = MAX_PACKET_LEN - currentLength;
-	}
-	unsigned char *buf;
-	if (size > 0 && (buf = (unsigned char *)calloc(1, size + 2)))
-	{
-		buf[0] = REG_FIFO | 0x80;
-		for (size_t i = 0; i < size; i++)
-		{
-			buf[i + 1] = data[i];
-		}
-		selectReceiver();
-		int bytes = wiringPiSPIDataRW(_spi_channel, buf, size + 1);
-		deselectReceiver();
-		writeRegister(REG_PAYLOAD_LENGTH, currentLength + size);
-		free(buf);
-		return bytes > 1 ? bytes - 1 : bytes;
-	}
-	return 0;
-}
-
-size_t LoRa::transmitPacket(LoRaPacket *packet)
-{
-	standby();
-	writeRegister(REG_FIFO_ADDR_PTR, 0x80);
-	writeRegister(REG_PAYLOAD_LENGTH, 0);
-	size_t size = packet->payloadLength();
-	unsigned char *payload = packet->getPayload();
-	while (size > 0)
-	{
-		size_t bytes = write(payload, size);
-		if (bytes == 0)
-			break;
-		payload += bytes;
-		size -= bytes;
-	}
-
-	// This test confirmed that the message is correctly placed into the FIFO buffer on the chip.
-	writeRegister(REG_FIFO_ADDR_PTR, 0x80);
-	unsigned char b[257] = {0};
-	for (int i = 0; i < 256; i++)
-	{
-		b[i] = readRegister(REG_FIFO);
-	}
-	printf("FIFO before tx:  %d %d %d %d %d %d %d %d \n", b[0],b[0],b[0],b[0],b[0],b[0],b[0],b[0]);
-	printf("END\n");
-	setOpMode(OPMODE_TX);
-
-	// Spin until transmission complete.
-	while ((readRegister(REG_IRQ_FLAGS) & IRQ_TXDONE_MASK) == 0)
-	{
-		usleep(1000);
-	}
-	writeRegister(REG_IRQ_FLAGS, IRQ_TXDONE_MASK);
-	sleep();
-	return packet->payloadLength() - size;
+    uint8_t bf[2] = {address | 0x80, value};
+    spi->write(bf, 2);
 }
--- lora/lora.hpp
+++ lora/lora.hpp
@@ -1,186 +1,182 @@
-#ifndef _LORA_H
-#define _LORA_H
+#ifndef _ARWAIN_LORA_HPP
+#define _ARWAIN_LORA_HPP
 
-#include <stdint.h>
-#include "packet.hpp"
+#include <string>
 
-#define OSC_FREQ                 32e6
-#define MAX_PACKET_LEN           0xFF
+#include "spi.hpp"
 
-#define REG_FIFO                 0x00
-#define REG_OPMODE               0x01
-// Reserved                      0x02
-// Reserved                      0x03
-// Reserved                      0x04
-// Reserved                      0x05
-#define REG_FRF_MSB              0x06
-#define REG_FRF_MID              0x07
-#define REG_FRF_LSB              0x08
-#define REG_PA_CONFIG            0x09
-#define REG_PA_RAMP              0x0A
-#define REG_OCP                  0x0B
-#define REG_LNA                  0x0C
-#define REG_FIFO_ADDR_PTR        0x0D
-#define REG_FIFO_TX_BASE_ADDR    0x0E
-#define REG_FIFO_RX_BASE_ADDR    0x0F
-#define REG_FIFO_RX_CURRENT_ADDR 0x10
-#define REG_IRQ_FLAGS_MASK       0x11
-#define REG_IRQ_FLAGS            0x12
-#define REG_RX_NB_BYTES          0x13
-#define REG_RX_HDR_CNT_MSB       0x14
-#define REG_RX_HDR_CNT_LSB       0x15
-#define REG_RX_PKT_CNT_MSB       0x16
-#define REG_RX_PKT_CNT_LSB       0x17
-#define REG_MODEM_STATUS         0x18
-#define REG_PACKET_SNR           0x19
-#define REG_PACKET_RSSI          0x1A
-#define REG_RSSI                 0x1B
-#define REG_HOP_CHANNEL          0x1C
-#define REG_MODEM_CONFIG1        0x1D
-#define REG_MODEM_CONFIG2        0x1E
-#define REG_SYMB_TIMEOUT_LSB     0x1F
-#define REG_PREAMBLE_MSB         0x20
-#define REG_PREAMBLE_LSB         0x21
-#define REG_PAYLOAD_LENGTH       0x22
-#define REG_MAX_PAYLOAD_LENGTH   0x23
-#define REG_HOP_PERIOD           0x24
-#define REG_FIFO_RX_BYTE_ADDR    0x25
-#define REG_MODEM_CONFIG3        0x26
-#define REG_PPM_CORRECTION       0x27
-#define REG_FEI_MSB              0x28
-#define REG_FEI_MID              0x29
-#define REG_FEI_LSB              0x2A
-// Reserved                      0x2B
-#define REG_RSSI_WIDEBAND        0x2C
-// Reserved                      0x2D
-// Reserved                      0x2E
-#define REG_IF_FREQ1             0x2F
-#define REG_IF_FREQ2             0x30
-#define REG_DETECT_OPTIMIZE      0x31
-// Reserved                      0x32
-#define REG_INVERT_IQ            0x33
-// Reserved                      0x34
-// Reserved                      0x35
-#define REG_HIGH_BW_OPTIM1       0x36
-#define REG_DETECT_THRESH        0x37
-// Reserved                      0x38
-#define REG_SYNC_WORD            0x39
-#define REG_HIGH_BW_OPTIM2       0x3A
-#define REG_INVERT_IQ2           0x3B
-// Reserved                      0x3C
-// Reserved                      0x3D
-// Reserved                      0x3E
-// Reserved                      0x3F
-#define REG_DIO_MAPPING1         0x40
-#define REG_DIO_MAPPING2         0x41
-#define REG_VERSION              0x42
-#define REG_TCXO                 0x4B
-#define REG_PA_DAC               0x4D
-#define REG_FORMER_TEMP          0x5B
-#define REG_AGC_REF              0x61
-#define REG_AGC_THRESH1          0x62
-#define REG_AGC_THRESH2          0x63
-#define REG_AGC_THRESH3          0x64
-#define REG_PLL                  0x70
+#define ADDRESS_WRITE_MODIFIER	(1<<7)
+#define FIFO_ADDRESS						(0x00)
+#define OPMODE_ADDRESS					(0x01)
+#define FRFMSB_ADDRESS					(0x06)
+#define FRFMIB_ADDRESS					(0x07)
+#define FRFLSB_ADDRESS					(0x08)
+#define PACONFIG_ADDRESS				(0x09)
+#define PARAMP_ADDRESS					(0x0a)
+#define OCP_ADDRESS							(0x0b)
+#define LNA_ADDRESS							(0x0c)
+#define FIFOPTR_ADDRESS					(0x0d)
+#define FIFOTXBASE_ADDRESS			(0x0e)
+#define FIFORXBASE_ADDRESS			(0x0f)
+#define FIFORXCURRENT_ADDRESS		(0x10)
+#define IRQFLAGSMASK_ADDRESS		(0x11)
+#define IRQFLAGS_ADDRESS				(0x12)
+#define RXNBBYTES_ADDRESS				(0x13)
+#define RXHDRCNTMSB_ADDRESS			(0x14)
+#define RXHDRCNTLSB_ADDRESS			(0x15)
+#define RXPACKETCNTMSB_ADDRESS	(0x16)
+#define RXPACKETCNTLSB_ADDRESS	(0x17)
+#define MODEMSTAT_ADDRESS				(0x18)
+#define PKTSNR_ADDRESS					(0x19)
+#define PKTRSSI_ADDRESS					(0x1a)
+#define RSSI_ADDRESS						(0x1b)
+#define HOPCHANNEL_ADDRESS			(0x1c)
+#define MODEMCONFIG1_ADDRESS		(0x1d)
+#define MODEMCONFIG2_ADDRESS		(0x1e)
+#define SYMBTIMEOUT_ADDRESS			(0x1f)
+#define PREAMBLELENMSB_ADDRESS	(0x20)
+#define PREAMBLELENLSB_ADDRESS	(0x21)
+#define PAYLOADLEN_ADDRESS			(0x22)
+#define MAXPAYLOADLEN_ADDRESS		(0x23)
+#define HOPPERIOD_ADDRESS				(0x24)
+#define FIFORXBYTEADDR_ADDRESS	(0x25)
+#define MODEMCONFIG3_ADDRESS		(0x26)
 
-#define PA_BOOST                 0x80
+#define OPMODE_LONGRANGE				(1<<7)
+#define OPMODE_ACCESSSHAREDREGS	(1<<6)
+#define OPMODE_LOWFREQMODEON		(1<<3)
+#define OPMODE_SLEEP						(0)
+#define OPMODE_STDBY						(1)
+#define OPMODE_FSTX							(2)
+#define OPMODE_TX								(3)
+#define OPMODE_FSRX							(4)
+#define OPMODE_RXCONTINUOUS			(5)
+#define OPMODE_RXSINGLE					(6)
+#define OPMODE_CAD							(7)
 
-// Radio operating modes
-#define OPMODE_LORA              0x80
-#define OPMODE_MASK              0x07
-#define OPMODE_SLEEP             0x00
-#define OPMODE_STANDBY           0x01
-#define OPMODE_FSTX              0x02
-#define OPMODE_TX                0x03
-#define OPMODE_FSRX              0x04
-#define OPMODE_RX                0x05
-#define OPMODE_RX_SINGLE         0x06
-#define OPMODE_CAD               0x07
+#define PACONFIG_PABOOST				(1<<7)
+#define PACONFIG_MAXPWR(x)			((x&7)<<4)
+#define PACONFIG_OUTPWR(x)			(x&0xf)
 
-#define IRQ_RXTOUT_MASK          0x80
-#define IRQ_RXDONE_MASK          0x40
-#define IRQ_CRCERR_MASK          0x20
-#define IRQ_HEADER_MASK          0x10
-#define IRQ_TXDONE_MASK          0x08
-#define IRQ_CDDONE_MASK          0x04
-#define IRQ_FHSSCH_MASK          0x02
-#define IRQ_CDDETD_MASK          0x01
+#define PARAMP_3_4MS						(0)
+#define PARAMP_2MS							(1)
+#define PARAMP_1MS							(2)
+#define PARAMP_500US						(3)
+#define PARAMP_250US						(4)
+#define PARAMP_125US						(5)
+#define PARAMP_100US						(6)
+#define PARAMP_62US							(7)
+#define PARAMP_50US							(8)
+#define PARAMP_40US							(9)
+#define PARAMP_31US							(10)
+#define PARAMP_25US							(11)
+#define PARAMP_20US							(12)
+#define PARAMP_15US							(13)
+#define PARAMP_12US							(14)
+#define PARAMP_10US							(15)
 
-// DIO function mappings                  D0D1D2D3
-#define MAP_DIO0_LORA_RXDONE     0x00  // 00------
-#define MAP_DIO0_LORA_TXDONE     0x40  // 01------
-#define MAP_DIO1_LORA_RXTOUT     0x00  // --00----
-#define MAP_DIO1_LORA_NOP        0x30  // --11----
-#define MAP_DIO2_LORA_NOP        0xC0  // ----11--
+#define OCP_ON									(1<<5)
+#define OCP_TRIM(x)							(x&0xf)
 
-class LoRa {
-	private:
-		unsigned char _spibuf[2];
-		uint8_t  _spi_channel;
-		uint8_t  _ss_pin;
-		uint8_t  _dio0_pin;
-		uint8_t  _rst_pin;
-		void selectReceiver();
-		void deselectReceiver();
-		uint8_t readRegister(uint8_t);
-		void writeRegister(uint8_t, uint8_t);
-		void setOpMode(uint8_t);
-		uint8_t getOpMode();
-		size_t receive(unsigned char *);
-		size_t write(const uint8_t *, size_t);
+#define LNA_GAIN_G1							(1<<5)			//MAX
+#define LNA_GAIN_G2							(2<<5)
+#define LNA_GAIN_G3							(3<<5)
+#define LNA_GAIN_G4							(4<<5)
+#define LNA_GAIN_G5							(5<<5)
+#define LNA_GAIN_G6							(6<<5)			//MIN
+#define LNA_BOOST_HF						(3)
 
-	public:
-		enum freq_t {
+#define IRQMASK_RXTIMEOUT				(1<<7)
+#define IRQMASK_RXDONE					(1<<6)
+#define IRQMASK_PAYLOADCRCERR		(1<<5)
+#define IRQMASK_VALIDHDR				(1<<4)
+#define IRQMASK_TXDONE					(1<<3)
+#define IRQMASK_CADDONE					(1<<2)
+#define IRQMASK_FHSSCHANCHANGE	(1<<1)
+#define IRQMASK_CADDETECTED			(1<<0)
+
+#define IRQ_RXTIMEOUT						(1<<7)
+#define IRQ_RXDONE							(1<<6)
+#define IRQ_PAYLOADCRCERR				(1<<5)
+#define IRQ_VALIDHDR						(1<<4)
+#define IRQ_TXDONE							(1<<3)
+#define IRQ_CADDONE							(1<<2)
+#define IRQ_FHSSCHANCHANGE			(1<<1)
+#define IRQ_CADDETECTED					(1<<0)
+
+#define MODEMSTAT_MODEMCLEAR		(1<<4)
+#define MODEMSTAT_HDRINFOVALID	(1<<3)
+#define MODEMSTAT_RXONGOING			(1<<2)
+#define MODEMSTAT_SIGSYNCED			(1<<1)
+#define MODEMSTAT_SIGDET				(1<<0)
+
+#define HOPCHANNEL_PLLTIMEOUT		(1<<7)
+#define HOPCHANNEL_RXCRCON			(1<<6)
+#define HOPCHANNEL_FHSSCHAN			(0x1f)
+
+#define MODEMCONFIG1_BW_7_8K		(0<<4)
+#define MODEMCONFIG1_BW_10_4K		(1<<4)
+#define MODEMCONFIG1_BW_15_6K		(2<<4)
+#define MODEMCONFIG1_BW_20_8K		(3<<4)
+#define MODEMCONFIG1_BW_31_25K	(4<<4)
+#define MODEMCONFIG1_BW_41_7K		(5<<4)
+#define MODEMCONFIG1_BW_62_5K		(6<<4)
+#define MODEMCONFIG1_BW_125K		(7<<4)
+#define MODEMCONFIG1_BW_250K		(8<<4)
+#define MODEMCONFIG1_BW_500K		(9<<4)
+#define MODEMCONFIG1_ECR_4_5		(1<<1)
+#define MODEMCONFIG1_ECR_4_6		(2<<1)
+#define MODEMCONFIG1_ECR_4_7		(3<<1)
+#define MODEMCONFIG1_ECR_4_8		(4<<1)
+#define MODEMCONFIG1_IMPLHDR		(1)
+
+#define MODEMCONFIG2_SF(x)			(x<<4)	//MUST be between 6 and 12
+#define MODEMCONFIG2_TXCONTMODE	(1<<3)
+#define MODEMCONFIG2_RXCRCON		(1<<2)
+#define MODEMCONFIG2_SYMTO_98		(3)
+
+#define MODEMCONFIG3_MOBILENODE	(1<<3)
+#define MODEMCONFIG3_AGCAUTOON	(1<<2)
+
+class LoRa
+{
+    public:
+        LoRa(const std::string& address);
+        ~LoRa();
+        uint8_t test_chip();
+		void send_message(const std::string& message);
+
+    private:
+        uint8_t read_register(uint8_t address);
+        void write_register(uint8_t address, uint8_t val);
+		void configure(const int frequency_mhz, const bool rxOnly);
+		void write_FIFO(const char* str, uint8_t num_bytes);
+
+    private:
+        SPI *spi = nullptr;
+        spi_config_t spi_config;
+
+    public: // Types
+        enum Frequency {
 			FREQ_433 = 433003300,
 			FREQ_868 = 868100000,
 			FREQ_915 = 915000000
 		};
 		static const uint32_t bw[10];
-		enum bw_t {
+		enum Bandwidth {
 			BW_7k8,  BW_10k4,  BW_15k6,
 			BW_20k8, BW_31k25, BW_41k7,
 			BW_62k5, BW_125k,  BW_250k,
 			BW_500k
 		};
-		enum sf_t { SF_6 = 6, SF_7, SF_8, SF_9, SF_10, SF_11, SF_12 };
-		enum hm_t { HM_EXPLICIT, HM_IMPLICIT };
-		enum cr_t { CR_45 = 1, CR_46, CR_47, CR_48 };
-		enum lna_gain_t {
+		enum SpreadFactor { SF_6 = 6, SF_7, SF_8, SF_9, SF_10, SF_11, SF_12 };
+		enum HeaderMode { HM_EXPLICIT, HM_IMPLICIT };
+		enum CodingRate { CR_45 = 1, CR_46, CR_47, CR_48 };
+		enum LNAGain {
 			LNA_G1 = 1, LNA_G2, LNA_G3,
 			LNA_G4,     LNA_G5, LNA_G6,
 			LNA_AGC
 		};
-
-		LoRa(uint8_t, uint8_t, uint8_t, uint8_t);
-		LoRa      *setSpreadFactor(sf_t);
-		LoRa      *setFrequency(uint32_t);
-		LoRa      *setBandwidth(bw_t);
-		LoRa      *setTXPower(uint8_t);
-		LoRa      *setHeaderMode(hm_t);
-		LoRa      *setCodingRate(cr_t);
-		LoRa      *setSyncWord(uint8_t);
-		LoRa      *setLNAGain(lna_gain_t);
-		LoRa      *setLNABoost(bool);
-		LoRa      *enableCRC();
-		LoRa      *disableCRC();
-		sf_t       getSpreadFactor();
-		uint32_t   getFrequency();
-		bw_t       getBandwidth();
-		uint8_t    getTXPower();
-		hm_t       getHeaderMode();
-		cr_t       getCodingRate();
-		uint8_t    getSyncWord();
-		lna_gain_t getLNAGain();
-		bool       getLNABoost();
-		int        getFrequencyError();
-
-		bool begin();
-		void sleep();
-		void standby();
-		uint8_t version();
-
-		LoRaPacket receivePacket();
-		size_t transmitPacket(LoRaPacket *);
 };
 
 #endif
