4265f91683bd5450038f5a65f730c1fc86d03774
Colin Broderick <colin.broderick@greeve.co.uk>
2021-11-25 13:39:01 +0000

    Converted drift sim to linear time algorithm, CA/CB

--- python_utils/drift_simulator.py
+++ python_utils/drift_simulator.py
@@ -1,38 +1,22 @@
+# import time
+# import math as mth
 # import matplotlib.pyplot as plt
-
-
-## TODO This procedure is currently a quadratic time algorithm. It can be made into a linear time algorithm.
-## In addition, when rotating a point, it is currently being rotated around the origin. It should be
-## rotated around the position of the previous point.
-
-
 import cmath as math
-
 
 def into_complex_single(n):
     n = n[0] + n[1] * 1j
     return n
-
 
 def into_complex(n):
     for i, val in enumerate(n):
         n[i] = into_complex_single(val)
     return n
 
-
 # adds dift to an array of positions in - needs to be given the angle of drift in degrees
-def add_drift_one_array(angle, element):
+def add_drift(angle, element):
     e = math.exp(1j * (angle * math.pi/180))
-    for i, val in enumerate(element):
-        element[i] = element[i] * e
+    element = element * e
     return element
-
-
-def add_drift(angle, x):
-    for i, val in enumerate(x):
-        x[i:] = add_drift_one_array(angle, x[i:])
-    return x
-
 
 def into_real(x):
     for i, val in enumerate(x):
@@ -42,18 +26,40 @@ def into_real(x):
 # simulates drift - requires an array of points and the angle of drift to be applied
 def simulate_drift(x, angle):
     x = into_complex(x)
-    x = add_drift(angle, x)
-    x = into_real(x)
-    return x
+    new_points = [x[0]]
+    for i in range(1, len(x)):
+        pt = x[i] - x[i-1]
+        pt = add_drift(i*angle, pt)
+        pt = pt + new_points[i-1]
+        new_points.append(pt)
 
+    new_points = into_real(new_points)
+    return new_points
 
-# simple example
-#points = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]
-# print(points)
-#points = simulate_drift(points, 1)
-# print(points)
+# # simple example
+# # points = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]
+# # points = [[i, i] for i in range(50)]
+# points = [
+#     [mth.sin(i/100.0), mth.cos(i/100.0)] for i in range(1, 36000)
+# ]
+
 # plt.scatter(
 #    [pt[0] for pt in points],
-#    [pt[1] for pt in points]
+#    [pt[1] for pt in points],
+#    label="Original path"
 # )
-# plt.show()
+
+# start = time.time()
+# drifted_points = simulate_drift(points, 0.1)
+# end = time.time()
+# print(end - start)
+
+
+# plt.scatter(
+#    [pt[0] for pt in drifted_points],
+#    [pt[1] for pt in drifted_points],
+#    label="Drifted path"
+# )
+# plt.axis("equal")
+# plt.legend()
+# plt.savefig("asdfasdf.png")
