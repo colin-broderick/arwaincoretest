5fb0b24011c99fa6f11b75b8fb8488ce7e1319cc
Colin Broderick <colin.broderick@greeve.co.uk>
2021-11-29 09:52:39 +0000

    .

--- src/imu_reader.cpp
+++ src/imu_reader.cpp
@@ -12,6 +12,7 @@
 #include "efaroe.hpp"
 #include "logger.hpp"
 #include "arwain.hpp"
+#include "timers.hpp"
 
 /** \brief Rotates a 3-vector according to a quaternion orientation.
  * \param vec The 3-vector to rotate.
@@ -78,39 +79,39 @@ void imu_reader()
         return;
     }
 
-    IMU_IIM42652 imu1{arwain::config.imu1_address, arwain::config.imu1_bus};
-    IMU_IIM42652 imu2{arwain::config.imu2_address, arwain::config.imu2_bus};
-    IMU_IIM42652 imu3{arwain::config.imu3_address, arwain::config.imu3_bus};
+    IMU_IIM42652 imu1{arwain::config.imu2_address, arwain::config.imu2_bus};
+    // IMU_IIM42652 imu2{arwain::config.imu2_address, arwain::config.imu2_bus};
+    // IMU_IIM42652 imu3{arwain::config.imu3_address, arwain::config.imu3_bus};
     // MLX90395 magnetometer{arwain::config.magn_address, arwain::config.magn_bus};
 
     // Choose an orientation filter depending on configuration, with Madgwick as default.
     arwain::Filter* filter1;
-    arwain::Filter* filter2;
-    arwain::Filter* filter3;
-    arwain::Filter* filter1_with_mag;
+    // arwain::Filter* filter2;
+    // arwain::Filter* filter3;
+    // arwain::Filter* filter1_with_mag;
     if (arwain::config.orientation_filter == "efaroe")
     {
         filter1 = new arwain::eFaroe{quaternion{0,0,0,0}, arwain::config.gyro1_bias, 100, arwain::config.efaroe_beta, arwain::config.efaroe_zeta};
-        filter2 = new arwain::eFaroe{quaternion{0,0,0,0}, arwain::config.gyro2_bias, 100, arwain::config.efaroe_beta, arwain::config.efaroe_zeta};
-        filter3 = new arwain::eFaroe{quaternion{0,0,0,0}, arwain::config.gyro3_bias, 100, arwain::config.efaroe_beta, arwain::config.efaroe_zeta};
-        filter1_with_mag = new arwain::eFaroe{quaternion{0,0,0,0}, arwain::config.gyro3_bias, 100, arwain::config.efaroe_beta, arwain::config.efaroe_zeta};
+        // filter2 = new arwain::eFaroe{quaternion{0,0,0,0}, arwain::config.gyro2_bias, 100, arwain::config.efaroe_beta, arwain::config.efaroe_zeta};
+        // filter3 = new arwain::eFaroe{quaternion{0,0,0,0}, arwain::config.gyro3_bias, 100, arwain::config.efaroe_beta, arwain::config.efaroe_zeta};
+        // filter1_with_mag = new arwain::eFaroe{quaternion{0,0,0,0}, arwain::config.gyro3_bias, 100, arwain::config.efaroe_beta, arwain::config.efaroe_zeta};
     }
     else if (arwain::config.orientation_filter == "madgwick")
     {
         filter1 = new arwain::Madgwick{1000.0/arwain::Intervals::IMU_READING_INTERVAL, arwain::config.madgwick_beta};
-        filter2 = new arwain::Madgwick{1000.0/arwain::Intervals::IMU_READING_INTERVAL, arwain::config.madgwick_beta};
-        filter3 = new arwain::Madgwick{1000.0/arwain::Intervals::IMU_READING_INTERVAL, arwain::config.madgwick_beta};
-        filter1_with_mag = new arwain::Madgwick{1000.0/arwain::Intervals::IMU_READING_INTERVAL, arwain::config.madgwick_beta};
+        // filter2 = new arwain::Madgwick{1000.0/arwain::Intervals::IMU_READING_INTERVAL, arwain::config.madgwick_beta};
+        // filter3 = new arwain::Madgwick{1000.0/arwain::Intervals::IMU_READING_INTERVAL, arwain::config.madgwick_beta};
+        // filter1_with_mag = new arwain::Madgwick{1000.0/arwain::Intervals::IMU_READING_INTERVAL, arwain::config.madgwick_beta};
     }
 
 
     // Local buffers for IMU data.
     vector3 accel_data1;
-    vector3 accel_data2;
-    vector3 accel_data3;
+    // vector3 accel_data2;
+    // vector3 accel_data3;
     vector3 gyro_data1;
-    vector3 gyro_data2;
-    vector3 gyro_data3;
+    // vector3 gyro_data2;
+    // vector3 gyro_data3;
     vector3 world_accel_data1;
     vector3 world_gyro_data1;
     euler_orientation_t euler_data;
@@ -145,7 +146,7 @@ void imu_reader()
         multi_quat_file << "time q1w q1x q1y q1z q2w q2x q2y q2z q3w q3x q3y q3z" << "\n";
     }
 
-    quaternion quat1, quat2, quat3, quat_aggregate, magnetovector;
+    quaternion quat1;//, quat2, quat3, quat_aggregate, magnetovector;
     int cycle_count = 0;
 
     // Set up timing.
@@ -155,28 +156,29 @@ void imu_reader()
 
     while (!arwain::shutdown)
     {
+        // arwain::Timers::ScopedTimer timer{"imu loop"};
         cycle_count++;
         timeCount = std::chrono::system_clock::now().time_since_epoch().count();
 
         imu1.read_IMU();
-        imu2.read_IMU();
-        imu3.read_IMU();
+        // imu2.read_IMU();
+        // imu3.read_IMU();
         // magnetovector = magnetometer.read_orientation();
 
         accel_data1 = {imu1.accelerometer_x, imu1.accelerometer_y, imu1.accelerometer_z};
         gyro_data1 = {imu1.gyroscope_x, imu1.gyroscope_y, imu1.gyroscope_z};
-        accel_data2 = {imu2.accelerometer_x, imu2.accelerometer_y, imu2.accelerometer_z};
-        gyro_data2 = {imu2.gyroscope_x, imu2.gyroscope_y, imu2.gyroscope_z};
-        accel_data3 = {imu3.accelerometer_x, imu3.accelerometer_y, imu3.accelerometer_z};
-        gyro_data3 = {imu3.gyroscope_x, imu3.gyroscope_y, imu3.gyroscope_z};
+        // accel_data2 = {imu2.accelerometer_x, imu2.accelerometer_y, imu2.accelerometer_z};
+        // gyro_data2 = {imu2.gyroscope_x, imu2.gyroscope_y, imu2.gyroscope_z};
+        // accel_data3 = {imu3.accelerometer_x, imu3.accelerometer_y, imu3.accelerometer_z};
+        // gyro_data3 = {imu3.gyroscope_x, imu3.gyroscope_y, imu3.gyroscope_z};
 
         // Adjust for biases according to configuration file.
         accel_data1 = accel_data1 - arwain::config.accel1_bias;
         gyro_data1 = gyro_data1 - arwain::config.gyro1_bias;
-        accel_data2 = accel_data2 - arwain::config.accel2_bias;
-        gyro_data2 = gyro_data2 - arwain::config.gyro2_bias;
-        accel_data3 = accel_data3 - arwain::config.accel3_bias;
-        gyro_data3 = gyro_data3 - arwain::config.gyro3_bias;
+        // accel_data2 = accel_data2 - arwain::config.accel2_bias;
+        // gyro_data2 = gyro_data2 - arwain::config.gyro2_bias;
+        // accel_data3 = accel_data3 - arwain::config.accel3_bias;
+        // gyro_data3 = gyro_data3 - arwain::config.gyro3_bias;
 
         { // Add new reading to end of buffer, and remove oldest reading from start of buffer.
             std::lock_guard<std::mutex> lock{arwain::Locks::IMU_BUFFER_LOCK};
@@ -196,22 +198,22 @@ void imu_reader()
             gyro_data1.x, gyro_data1.y, gyro_data1.z,
             accel_data1.x, accel_data1.y, accel_data1.z
         );
-        filter2->update(
-            timeCount,
-            gyro_data2.x, gyro_data2.y, gyro_data2.z,
-            accel_data2.x, accel_data2.y, accel_data2.z
-        );
-        filter3->update(
-            timeCount,
-            gyro_data3.x, gyro_data3.y, gyro_data3.z,
-            accel_data3.x, accel_data3.y, accel_data3.z
-        );
+        // filter2->update(
+        //     timeCount,
+        //     gyro_data2.x, gyro_data2.y, gyro_data2.z,
+        //     accel_data2.x, accel_data2.y, accel_data2.z
+        // );
+        // filter3->update(
+        //     timeCount,
+        //     gyro_data3.x, gyro_data3.y, gyro_data3.z,
+        //     accel_data3.x, accel_data3.y, accel_data3.z
+        // );
 
         // SLERP all orientation filters into a canonical filter.
         quat1 = {filter1->getW(), filter1->getX(), filter1->getY(), filter1->getZ()};
-        quat2 = {filter2->getW(), filter2->getX(), filter2->getY(), filter2->getZ()};
-        quat3 = {filter3->getW(), filter3->getX(), filter3->getY(), filter3->getZ()};
-        quat_aggregate = quaternion::nslerp(quaternion::nslerp(quat1, quat2, 0.5), quat3, 0.6666666);
+        // quat2 = {filter2->getW(), filter2->getX(), filter2->getY(), filter2->getZ()};
+        // quat3 = {filter3->getW(), filter3->getX(), filter3->getY(), filter3->getZ()};
+        // quat_aggregate = quaternion::nslerp(quaternion::nslerp(quat1, quat2, 0.5), quat3, 0.6666666);
 
         // Compute the SLERP factor based on how 'wrong' the magnetic orientation is compared to the gyro orientation.
         // double s = gyro_mag_co_trust(quat_aggregate, magnetovector);
@@ -226,23 +228,23 @@ void imu_reader()
         // filter2->setQ(quat2.w, quat2.x, quat2.y, quat2.z);
         // filter3->setQ(quat3.w, quat3.x, quat3.y, quat3.z);
 
-        if (cycle_count % 100 == 0)
-        {
-            multi_quat_file << timeCount << " "
-                            << filter1->getW() << " " << filter1->getX() << " " << filter1->getY() << " " << filter1->getZ() << " "
-                            << filter2->getW() << " " << filter2->getX() << " " << filter2->getY() << " " << filter2->getZ() << " "
-                            << filter3->getW() << " " << filter3->getX() << " " << filter3->getY() << " " << filter3->getZ() << "\n";
-        }
+        // if (cycle_count % 100 == 0)
+        // {
+        //     multi_quat_file << timeCount << " "
+        //                     << filter1->getW() << " " << filter1->getX() << " " << filter1->getY() << " " << filter1->getZ() << " "
+        //                     << filter2->getW() << " " << filter2->getX() << " " << filter2->getY() << " " << filter2->getZ() << " "
+        //                     << filter3->getW() << " " << filter3->getX() << " " << filter3->getY() << " " << filter3->getZ() << "\n";
+        // }
 
         // Extract Euler orientation from filter.
-        euler_data = compute_euler(quat_aggregate);
+        euler_data = compute_euler(quat1);
 
         { // Add orientation information to buffers.
             std::lock_guard<std::mutex> lock{arwain::Locks::ORIENTATION_BUFFER_LOCK};
             arwain::Buffers::EULER_ORIENTATION_BUFFER.pop_front();
             arwain::Buffers::EULER_ORIENTATION_BUFFER.push_back(euler_data);
             arwain::Buffers::QUAT_ORIENTATION_BUFFER.pop_front();
-            arwain::Buffers::QUAT_ORIENTATION_BUFFER.push_back(quat_aggregate);
+            arwain::Buffers::QUAT_ORIENTATION_BUFFER.push_back(quat1);
         }
 
         // Add world-aligned IMU to its own buffer.
@@ -263,7 +265,7 @@ void imu_reader()
         if (arwain::config.log_to_file)
         {
             euler_file << timeCount << " " << euler_data.roll << " " << euler_data.pitch << " " << euler_data.yaw << "\n";
-            quat_file << timeCount << " " << quat_aggregate.w << " " << quat_aggregate.x << " " << quat_aggregate.y << " " << quat_aggregate.z << "\n";
+            quat_file << timeCount << " " << quat1.w << " " << quat1.x << " " << quat1.y << " " << quat1.z << "\n";
         }
 
         // Wait until the next tick.
@@ -272,8 +274,8 @@ void imu_reader()
     }
 
     delete filter1;
-    delete filter2;
-    delete filter3;
+    // delete filter2;
+    // delete filter3;
 
     // Close all file handles.
     if (arwain::config.log_to_file)
