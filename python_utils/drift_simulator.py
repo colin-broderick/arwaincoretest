e54517fce6541a0ae1ca161b7a4d6bdc3c729181
Colin Broderick <colin.broderick@greeve.co.uk>
2022-01-26 14:08:38 +0000

    Finished automatic drift correction in dashboard

--- python_utils/drift_simulator.py
+++ python_utils/drift_simulator.py
@@ -1,20 +1,56 @@
-# import time
-# import math as mth
-# import matplotlib.pyplot as plt
-import cmath as math
+import math
+import cmath
+
+def closed_path_error(points):
+    return math.sqrt((points[0][0] - points[-1][0])**2 + (points[0][1] - points[-1][1])**2)
+
+def sign(number):
+    return number >= 0
+
+def find_best_drift_correction(points):
+    """
+    Assuming a closed path, find the artificial drift which best closes the path.
+    """
+    error = closed_path_error(points)
+    switches = 5
+    drift = 1.0/60.0/20.0
+    drift_delta = 1.0/60.0/20.0
+    print("Start error:", error)
+    first_try = True
+    print("Drift:", drift, "    New error:", "N/A", "     delta:", drift_delta)
+
+    while switches > 0:
+        new_points = simulate_drift(points, drift)
+        new_error = closed_path_error(new_points)
+        if new_error > error:
+            if first_try:
+                drift *= -1
+                drift_delta *= -1
+            else:
+                drift_delta /= -2.0
+                drift += drift_delta
+                switches -= 1
+        elif new_error < error:
+            error = new_error
+            drift += drift_delta
+        first_try = False
+        print("Drift:", drift, "    New error:", new_error, "     delta:", drift_delta)
+
+    return new_points, drift*60*20
 
 def into_complex_single(n):
     n = n[0] + n[1] * 1j
     return n
 
 def into_complex(n):
+    new_list = []
     for i, val in enumerate(n):
-        n[i] = into_complex_single(val)
-    return n
+        new_list.append(into_complex_single(val))
+    return new_list
 
 # adds dift to an array of positions in - needs to be given the angle of drift in degrees
 def add_drift(angle, element):
-    e = math.exp(1j * (angle * math.pi/180))
+    e = cmath.exp(1j * (angle * cmath.pi/180))
     element = element * e
     return element
 
@@ -25,41 +61,13 @@ def into_real(x):
 
 # simulates drift - requires an array of points and the angle of drift to be applied
 def simulate_drift(x, angle):
-    x = into_complex(x)
-    new_points = [x[0]]
-    for i in range(1, len(x)):
-        pt = x[i] - x[i-1]
+    y = into_complex(x)
+    new_points = [y[0]]
+    for i in range(1, len(y)):
+        pt = y[i] - y[i-1]
         pt = add_drift(i*angle, pt)
         pt = pt + new_points[i-1]
         new_points.append(pt)
 
     new_points = into_real(new_points)
     return new_points
-
-# # simple example
-# # points = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]
-# # points = [[i, i] for i in range(50)]
-# points = [
-#     [mth.sin(i/100.0), mth.cos(i/100.0)] for i in range(1, 36000)
-# ]
-
-# plt.scatter(
-#    [pt[0] for pt in points],
-#    [pt[1] for pt in points],
-#    label="Original path"
-# )
-
-# start = time.time()
-# drifted_points = simulate_drift(points, 0.1)
-# end = time.time()
-# print(end - start)
-
-
-# plt.scatter(
-#    [pt[0] for pt in drifted_points],
-#    [pt[1] for pt in drifted_points],
-#    label="Drifted path"
-# )
-# plt.axis("equal")
-# plt.legend()
-# plt.savefig("asdfasdf.png")
