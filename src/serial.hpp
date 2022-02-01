009d367ce55cc35c394a3ea0ebd977ee082796a2
Colin Broderick <colin.broderick@greeve.co.uk>
2022-02-01 19:24:36 +0000

    Serial port class added (from UUBLA)

---
+++ src/serial.hpp
@@ -0,0 +1,91 @@
+/***************************************************************************
+
+	Header-only serial port reader which wraps boost::asio::serial_port.
+	
+	Author: CB, Greeve Ltd
+
+***************************************************************************/
+
+#ifndef _GREEVE_UUBLA_SERIAL_H
+#define _GREEVE_UUBLA_SERIAL_H
+
+#define BOOST_ALL_NO_LIB
+
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/asio/serial_port.hpp>
+
+class Serial
+{
+	public:
+		const static int MAX_LINE_LEN = 120;
+	
+	public:
+		/** \brief Constructor. Opens and configures serial port.
+		 * \param[in] com_port String defining the port (e.g. "COM14", "/dev/ttyS0").
+		 * \param[in] baudrate Speed of the serial port (e.g. 9600, 115200).
+		 */
+		Serial(const std::string& com_port, const int baudrate)
+		{
+			this->port.open(com_port);
+			this->port.set_option(boost::asio::serial_port_base::baud_rate(baudrate));
+			this->port_open = true;
+		}
+
+		/** \brief Send a string to the serial port.
+		 * \param message Content of the message; make sure to include EOL character(s) as required.
+		 */
+        void write(const std::string& message)
+        {
+            boost::asio::write(this->port, boost::asio::buffer(message.c_str(), message.size()));
+        }
+
+		/** \brief Reads a line from the serial port, up to a maximum length
+		 * given by Serial::MAX_LINE_LEN.
+		 * \return std::string as read from serial port.
+		 */
+		std::string readline()
+		{
+			// TODO This needs some timeout behaviour or it will block forever when nothing received on serial port.
+			char buffer[Serial::MAX_LINE_LEN + 2];
+			char c = 0;
+			int index = 0;
+			while (index < Serial::MAX_LINE_LEN)
+			{
+				boost::asio::read(this->port, boost::asio::buffer(&c, 1));
+				if (c == '\r' || c  == '\n')
+				{
+					boost::asio::read(this->port, boost::asio::buffer(&c, 1));
+					break;
+				}
+				buffer[index] = c;
+				index++;
+			}
+			buffer[index] = 0;
+			return std::string{buffer};
+		}
+
+		/** \brief Destructor. Ensures the port is closed before program exit. */
+		~Serial()
+		{
+			this->close();
+		}
+
+	private:
+		boost::asio::io_service io;
+		boost::asio::serial_port port{io};
+		bool port_open = false;
+
+	private:
+		/** \brief Close serial port handle. */
+		void close()
+		{
+			if (this->port_open)
+			{
+				this->port_open = false;
+				this->port.close();
+			}
+		}
+};
+
+#endif
