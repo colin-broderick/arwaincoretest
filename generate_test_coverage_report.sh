15697df89eb32492acbf9cf06cdbda5189dc7324
Colin Broderick <colin.broderick@greeve.co.uk>
2022-10-24 12:29:35 +0100

    Added test report coverage

---
+++ generate_test_coverage_report.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir ./test_report
+lcov --capture --directory ./CMakeFiles/arwain_test.dir/src/ --output-file=coverage.info
+genhtml coverage.info --output-directory=test_report
+
+echo "Test reports placed in PROJECT_DIR/build/test_report/. Run an http server from that location to view in browser."
+echo "This can be done by, for example:"
+echo "    python3 -m http.server"
+
