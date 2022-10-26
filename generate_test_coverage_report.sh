a5f7a8f6c3c749de1d90446cfbbaf262f40e7d98
Colin Broderick <colin.broderick@greeve.co.uk>
2022-10-26 14:21:08 +0000

    First few unit tests

--- generate_test_coverage_report.sh
+++ generate_test_coverage_report.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir ./test_report
-lcov --capture --directory ./CMakeFiles/arwain_test.dir/src/ --output-file=coverage.info
+lcov --capture --directory ./CMakeFiles/arwain_test.dir/src/ --directory ./CMakeFiles/arwain_test.dir/quaternion/ --output-file=coverage.info
 genhtml coverage.info --output-directory=test_report
 
 echo "Test reports placed in PROJECT_DIR/build/test_report/. Run an http server from that location to view in browser."
