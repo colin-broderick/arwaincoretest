f78449aa8ae93bb5e21ab985126429a6844ca686
Colin Broderick <colin.broderick@greeve.co.uk>
2022-10-28 09:40:35 +0000

    Richer doxygen in tests.cpp. Expanded Test_QuaternionInverse() to cover more cases.

--- src/tests.cpp
+++ src/tests.cpp
@@ -8,6 +8,9 @@
 
 namespace Random
 {
+    /** \brief Generates a random double in the interval [-10, 10).
+     * \return Double in [-10, 10).
+     */
     double Double()
     {
         static std::random_device rd;
@@ -15,6 +18,12 @@ namespace Random
         static std::uniform_real_distribution<double> dist(-10.0, 10.0);
         return dist(mt);
     }
+
+    /** \brief Generates a random double in the interval [lower_bound, upper_bound). 
+     * \param lower_bound Smallest value that can be returned.
+     * \param upper_bound Lowest upper bound on returnable values.
+     * \return Double in [lower_bound, upper_bound).
+     */
     double DoubleBetween(const double lower_bound, const double upper_bound)
     {
         static std::random_device rd;
@@ -24,22 +33,36 @@ namespace Random
     }
 }
 
+/** \brief Return this to fail a test.
+ * \return Integer 1, to indicate failed execution in UNIX-like environment.
+ */
 int fail_test()
 {
     return 1;
 }
 
+/** \brief Return this to pass a test.
+ * \return Integer 0, to indicate successful execution in UNIX-like environment.
+ */
 int pass_test()
 {
     return 0;
 }
 
+/** \brief Checks that two doubles are 'equal' to within a certain (optional specified) tolerances.
+ * \param num1 First double to compare.
+ * \param num2 Second double to compare.
+ * \param tolerance [optional] The two doubles should differ by no more than this to be considered equal.
+ * \return Boolean indicating whether the two doubles are equal according to the given definition.
+ */
 bool is_close(double num1, double num2, double tolerance = 0.0000001)
 {
     return std::abs(num1 - num2) < tolerance;
 }
 
-/** \brief Testing that the unit test framework is configured correctly. */
+/** \brief Testing that the unit test framework is configured correctly.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_Test()
 {
     return pass_test();
@@ -47,7 +70,9 @@ int Test_Test()
 
 /* -------------------------------------- QUATERNION_TESTS ------------------------------------- */
 
-/** \brief Test that the 4-element quaternion constructor produces valid output. */
+/** \brief Test that the 4-element quaternion constructor produces valid output.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionConstructors()
 {
     bool passing = true;
@@ -70,6 +95,10 @@ int Test_QuaternionConstructors()
     return passing ? pass_test() : fail_test();
 }
 
+/** \brief The inverse of a versor is equal to its conjugate. The general inverse should agree with that given by
+ * wolframalpha, e.g. https://www.wolframalpha.com/input/?i=inverse+of+quaternion+1%2B0i%2B0j%2B2k+**+%28-1i%2B3%2B4j%2B3k%29
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionInverse()
 {
     bool passing = true;
@@ -87,10 +116,53 @@ int Test_QuaternionInverse()
         passing &= is_close(q1i.z, q1c.z);
     }
 
+    // General quaternion inverse, as compard to WolframAlpha result.
+    Quaternion q1{0.089732334, 0.036265566, 0.4167531, 0.878915397};
+    Quaternion q1i = q1.inverse();
+    Quaternion q1i_expected{0.0939072, -0.0379529, -0.436143, -0.919808};
+    passing &= is_close(q1i.w, q1i_expected.w, 0.0001);
+    passing &= is_close(q1i.x, q1i_expected.x, 0.0001);
+    passing &= is_close(q1i.y, q1i_expected.y, 0.0001);
+    passing &= is_close(q1i.z, q1i_expected.z, 0.0001);
+
+    Quaternion q2{0.143179759, 0.53385755, 0.366605684, 0.100738656};
+    Quaternion q2i = q2.inverse();
+    Quaternion q2i_expected{0.31814, -1.18621, -0.814585, -0.223838};
+    passing &= is_close(q2i.w, q2i_expected.w, 0.0001);
+    passing &= is_close(q2i.x, q2i_expected.x, 0.0001);
+    passing &= is_close(q2i.y, q2i_expected.y, 0.0001);
+    passing &= is_close(q2i.z, q2i_expected.z, 0.0001);
+    
+    Quaternion q3{0.004420628, 0.616040595, 0.840586052, 0.798462594};
+    Quaternion q3i = q3.inverse();
+    Quaternion q3i_expected{0.00256469, -0.357404, -0.487677, -0.463239};
+    passing &= is_close(q3i.w, q3i_expected.w, 0.0001);
+    passing &= is_close(q3i.x, q3i_expected.x, 0.0001);
+    passing &= is_close(q3i.y, q3i_expected.y, 0.0001);
+    passing &= is_close(q3i.z, q3i_expected.z, 0.0001);
+    
+    Quaternion q4{0.463493268, 0.516989217, 0.820006171, 0.978068835};
+    Quaternion q4i = q4.inverse();
+    Quaternion q4i_expected{0.219547, -0.244887, -0.38842, -0.463291};
+    passing &= is_close(q4i.w, q4i_expected.w, 0.0001);
+    passing &= is_close(q4i.x, q4i_expected.x, 0.0001);
+    passing &= is_close(q4i.y, q4i_expected.y, 0.0001);
+    passing &= is_close(q4i.z, q4i_expected.z, 0.0001);
+    
+    Quaternion q5{0.701366791, 0.938252281, 0.837144943, 0.37816936};
+    Quaternion q5i = q5.inverse();
+    Quaternion q5i_expected{0.316493, -0.423388, -0.377763, -0.17065};
+    passing &= is_close(q5i.w, q5i_expected.w, 0.0001);
+    passing &= is_close(q5i.x, q5i_expected.x, 0.0001);
+    passing &= is_close(q5i.y, q5i_expected.y, 0.0001);
+    passing &= is_close(q5i.z, q5i_expected.z, 0.0001);
+    
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief Quaternions add element-wise. */
+/** \brief Quaternions add element-wise.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionSum()
 {
     bool passing = true;
@@ -112,6 +184,7 @@ int Test_QuaternionSum()
 /** \brief A rotor quaternion can be constructed from a real number and a 3-vector.
  * The real number is the angle by which the rotor will rotate a vector, and the 3-vector
  * is the axis about which the rotation is performed.
+ * \return 0 for test pass, 1 for test fail.
  */
 int Test_QuaternionAxisAngleConstructor()
 {
@@ -150,7 +223,9 @@ int Test_QuaternionAxisAngleConstructor()
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief The default quaternion constructor creates the unit real quaternion. */
+/** \brief The default quaternion constructor creates the unit real quaternion.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionDefaultConstructor()
 {
     bool passing = true;
@@ -173,6 +248,7 @@ int Test_QuaternionDefaultConstructor()
 
 /** \brief A quaternion can be constructed from a vector; it will have zero real part, and the vector part
  * will be equal to the supplied vector.
+ * \return 0 for test pass, 1 for test fail.
  */
 int Test_QuaternionVectorConstructor()
 {
@@ -191,7 +267,9 @@ int Test_QuaternionVectorConstructor()
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief The quaternion dot product is the same as for other vectors, i.e. the sum of element-wise products. */
+/** \brief The quaternion dot product is the same as for other vectors, i.e. the sum of element-wise products.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionDotProduct()
 {
     bool passing = true;
@@ -211,6 +289,7 @@ int Test_QuaternionDotProduct()
 
 /** \brief Test computation of the angle between two rotor quaternions. Note that this concept
  * is only defined for unit quaternions.
+ * \return 0 for test pass, 1 for test fail.
  */
 int Test_QuaternionAngleBetween()
 {
@@ -232,7 +311,9 @@ int Test_QuaternionAngleBetween()
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief Test subtraction of two quaternions. Quaternion subtraction is element-wise. */
+/** \brief Test subtraction of two quaternions. Quaternion subtraction is element-wise.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionSubtractionOperator()
 {
     bool passing = true;
@@ -253,7 +334,9 @@ int Test_QuaternionSubtractionOperator()
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief Test unary subtraction i.e. negation of a quaternion. All elements are negated. */
+/** \brief Test unary subtraction i.e. negation of a quaternion. All elements are negated.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionUnarySubtraction()
 {
     bool passing = true;
@@ -272,7 +355,9 @@ int Test_QuaternionUnarySubtraction()
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief A unit quaternion has norm of one, within the tolerances of floating point arithmetic. */
+/** \brief A unit quaternion has norm of one, within the tolerances of floating point arithmetic.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionIsNormal()
 {
     bool passing = true;
@@ -290,7 +375,9 @@ int Test_QuaternionIsNormal()
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief The vector part of a quaternion is simply a 3-vector formed of elements [x, y, z]. */
+/** \brief The vector part of a quaternion is simply a 3-vector formed of elements [x, y, z].
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionVectorPart()
 {
     bool passing = true;
@@ -307,7 +394,9 @@ int Test_QuaternionVectorPart()
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief Two quaternions are equal if all of their elements are equal. */
+/** \brief Two quaternions are equal if all of their elements are equal.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionEqualityOperator()
 {
     bool passing = true;
@@ -355,7 +444,9 @@ int Test_QuaternionEqualityOperator()
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief We can send a string representation of a quaternion using the stream operators. */
+/** \brief We can send a string representation of a quaternion using the stream operators.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionOutputStreamOperator()
 {
     bool passing = true;
@@ -378,6 +469,7 @@ int Test_QuaternionOutputStreamOperator()
 
 /** \brief The quaternion product is defined here:
  * https://colin-broderick.medium.com/deriving-the-quaternion-product-a22858c40921
+ * \return 0 for test pass, 1 for test fail.
  */
 int Test_QuaternionProduct()
 {
@@ -417,6 +509,7 @@ int Test_QuaternionProduct()
 /** \brief We generate some quaternions, and test that the slerp of them is in agreement with an
  * an independent calculation. The independent calculator is here (on 27/10/2022):
  * https://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/slerp/index.htm
+ * \return 0 for test pass, 1 for test fail.
  */
 int Test_QuaternionSlerp()
 {
@@ -467,7 +560,9 @@ int Test_QuaternionSlerp()
     return passing ? pass_test() : fail_test();
 }
 
-/** \brief Quaternion::nslerp calls Quaternion::slerp, then normalizes the result before returning. */
+/** \brief Quaternion::nslerp calls Quaternion::slerp, then normalizes the result before returning.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int Test_QuaternionNormSlerp()
 {
     bool passing = true;
@@ -557,6 +652,13 @@ int Test_InputParserContainerError()
     }
 }
 
+/** \brief Defines and execute tests according to string command line argument.
+ * Should be called as
+ *     ./binary_name Test_NameOfTest
+ * \param argc Argument count.
+ * \param argv Array of command line arguments.
+ * \return 0 for test pass, 1 for test fail.
+ */
 int main(int argc, char* argv[])
 {
     std::map<std::string, std::function<int()>> funcs = {
