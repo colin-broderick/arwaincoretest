f7ebe919fe660aaaa28b61ac02c41bcc3e572c69
Cobain Attwood <cobain.attwood@greeve.co.uk>
2022-11-01 13:09:41 +0000

    madgwick unit tests started to be added

--- generate_test_coverage_report.sh
+++ generate_test_coverage_report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir ./test_report
-lcov --capture --directory ./CMakeFiles/arwain_test.dir/src/ --directory ./CMakeFiles/arwain_test.dir/quaternion/ --directory ./CMakeFiles/arwain_test.dir/logger/ --output-file=coverage.info
+lcov --capture --directory ./CMakeFiles/arwain_test.dir/quaternion/ --directory ./CMakeFiles/arwain_test.dir/logger/  --directory ./CMakeFiles/arwain_test.dir/input_parser/ --directory ./CMakeFiles/arwain_test.dir/orientation/ --output-file=coverage.info
 genhtml coverage.info --output-directory=test_report 
 
 echo "Test reports placed in PROJECT_DIR/build/test_report/. Run an http server from that location to view in browser."
