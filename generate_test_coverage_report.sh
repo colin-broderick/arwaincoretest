4a758d4393c5ff556d1e53fc9e1e1192e5765438
Colin Broderick <colin.broderick@greeve.co.uk>
2022-11-01 15:35:05 +0000

    Added vector3 to test coverage reports

--- generate_test_coverage_report.sh
+++ generate_test_coverage_report.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 mkdir ./test_report
-lcov --capture --directory ./CMakeFiles/arwain_test.dir/quaternion/ --directory ./CMakeFiles/arwain_test.dir/logger/  --directory ./CMakeFiles/arwain_test.dir/input_parser/ --directory ./CMakeFiles/arwain_test.dir/orientation/ --output-file=coverage.info
+lcov --capture \
+     --directory ./CMakeFiles/arwain_test.dir/quaternion/ \
+     --directory ./CMakeFiles/arwain_test.dir/logger/ \
+     --directory ./CMakeFiles/arwain_test.dir/input_parser/ \
+     --directory ./CMakeFiles/arwain_test.dir/orientation/ \
+     --directory ./CMakeFiles/arwain_test.dir/vector3/ \
+     --output-file=coverage.info
 genhtml coverage.info --output-directory=test_report 
 
 echo "Test reports placed in PROJECT_DIR/build/test_report/. Run an http server from that location to view in browser."
