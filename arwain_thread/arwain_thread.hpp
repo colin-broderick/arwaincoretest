2acc9d60de42846b07b9c996222622af2fcde813
Colin Broderick <colin.broderick@greeve.co.uk>
2022-11-03 11:45:11 +0000

    Completed doxygen for arwain_thread

--- arwain_thread/arwain_thread.hpp
+++ arwain_thread/arwain_thread.hpp
@@ -4,12 +4,15 @@
 #include <thread>
 #include <vector>
 
+/** \brief A wrapper around std::thread which provides the ability to set thread name and core affinity. */
 class ArwainThread : public std::thread
 {
-
     public:
         const static int AllCores = -1;
 
+        /** \brief Does exactly what std::thread does.
+         * \param func A callable object (generally a function), which will be run in a new thread.
+         */
         template <class Callable>
         ArwainThread(Callable func)
         : std::thread(func)
@@ -17,6 +20,11 @@ class ArwainThread : public std::thread
 
         };
 
+        /** \brief Creates a thread, and assigned a name and core affinity.
+         * \param func A callable object (generally a function), which will be run in a new thread.
+         * \param name_ A name for the thread, max 16 characters.
+         * \param cores A vector of ints, where the ints index the cores to be assigned to the task, starting at zero.
+         */
         template <class Callable>
         ArwainThread(Callable func, const std::string& name_, const std::vector<int>&& cores)
         : std::thread(func)
@@ -25,6 +33,12 @@ class ArwainThread : public std::thread
             this->set_processor_affinity(cores);
         };
 
+        /** \brief Creates a thread, and assigned a name and core affinity.
+         * \param func A callable object (generally a function), which will be run in a new thread.
+         * \param name_ A name for the thread, max 16 characters.
+         * \param cores A vector of ints, where the ints index the cores to be assigned to the task, starting at zero.
+         * \param args A collection of arguments to pass to the threaded .
+         */
         template <class Callable, class... Args>
         ArwainThread(Callable func, const std::string& name_, const std::vector<int>&& cores, Args&&... args)
         : std::thread(func, args...)
@@ -38,7 +52,6 @@ class ArwainThread : public std::thread
     private:
         void set_processor_affinity(const std::vector<int>& cores);
         void set_name(const std::string& name);
-        std::string name;
 };
 
 #endif
