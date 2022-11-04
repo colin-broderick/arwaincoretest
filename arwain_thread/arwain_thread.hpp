d2b9dab93f44a460e5f55730ba4182c3a4a19662
Colin Broderick <colin.broderick@greeve.co.uk>
2022-11-04 20:29:05 +0000

    Scrapped failed attempt at thread affinity; will be added as future feature

--- arwain_thread/arwain_thread.hpp
+++ arwain_thread/arwain_thread.hpp
@@ -4,12 +4,10 @@
 #include <thread>
 #include <vector>
 
-/** \brief A wrapper around std::thread which provides the ability to set thread name and core affinity. */
+/** \brief A wrapper around std::thread which provides the ability to set thread name. */
 class ArwainThread : public std::thread
 {
     public:
-        const static int AllCores = -1;
-
         /** \brief Does exactly what std::thread does.
          * \param func A callable object (generally a function), which will be run in a new thread.
          */
@@ -20,37 +18,32 @@ class ArwainThread : public std::thread
 
         };
 
-        /** \brief Creates a thread, and assigned a name and core affinity.
+        /** \brief Creates a thread, and assigns a name.
          * \param func A callable object (generally a function), which will be run in a new thread.
          * \param name_ A name for the thread, max 16 characters.
-         * \param cores A vector of ints, where the ints index the cores to be assigned to the task, starting at zero.
          */
         template <class Callable>
-        ArwainThread(Callable func, const std::string& name_, const std::vector<int>&& cores)
+        ArwainThread(Callable func, const std::string& name_)
         : std::thread(func)
         {
             this->set_name(name_);
-            this->set_processor_affinity(cores);
         };
 
-        /** \brief Creates a thread, and assigned a name and core affinity.
+        /** \brief Creates a thread, and assignes a name.
          * \param func A callable object (generally a function), which will be run in a new thread.
          * \param name_ A name for the thread, max 16 characters.
-         * \param cores A vector of ints, where the ints index the cores to be assigned to the task, starting at zero.
          * \param args A collection of arguments to pass to the threaded .
          */
         template <class Callable, class... Args>
-        ArwainThread(Callable func, const std::string& name_, const std::vector<int>&& cores, Args&&... args)
+        ArwainThread(Callable func, const std::string& name_, Args&&... args)
         : std::thread(func, args...)
         {
             this->set_name(name_);
-            this->set_processor_affinity(cores);
         };
 
         std::string get_name();
 
     private:
-        void set_processor_affinity(const std::vector<int>& cores);
         void set_name(const std::string& name);
 };
 
