56f61ee833f9fd2cf2f068f877db7a82eb5064e9
Colin Broderick <colin.broderick@greeve.co.uk>
2022-11-09 14:13:28 +0000

    Sketched the pressure sensor simulator

--- bmp384/bmp384.hpp
+++ bmp384/bmp384.hpp
@@ -1,6 +1,68 @@
 #ifndef _BMP384_DRIVER_HPP
 #define _BMP384_DRIVER_HPP
 
+#if FAKEI2C
+
+// CLASS --------------------------------------------------------------
+
+struct CalibData
+{
+    double t_lin;
+    double par_t1;
+    double par_t2;
+    double par_t3;
+    double par_p1;
+    double par_p2;
+    double par_p3;
+    double par_p4;
+    double par_p5;
+    double par_p6;
+    double par_p7;
+    double par_p8;
+    double par_p9;
+    double par_p10;
+    double par_p11;
+};
+
+class BMP384
+{
+    public:
+        const static int min_temp = -40;
+        const static int max_temp = 85;
+        enum class Mode { USE_HYPS, USE_SIMPLE };
+
+    public:
+        BMP384(int bus_address, const std::string &bus_name);
+        std::tuple<double, double> read();
+        int get_chip_id();
+        static double calculate_altitude(const double pressure, const double temperature, const double sea_level_pressure);
+        static double hypsometric_altitude(double pressure, double temperature, double sea_level_pressure);
+        static double simple_altitude(double pressure, double sea_level_pressure);
+        void set_altitude_mode(Mode mode);
+
+    private:
+        void set_power_mode(uint8_t mode);
+        void i2c_init(const int address, const std::string &bus_name);
+        int i2c_read(int reg_addr, int bytes, uint8_t *buffer);
+        int i2c_write(int reg_addr, int bytes, uint8_t *buffer);
+        void enable_pressure();
+        void enable_temperature();
+        void set_pressure_osr(uint8_t osr);
+        void set_temperature_osr(uint8_t osr);
+        void set_iir_filter(uint8_t coefficient);
+        void soft_reset();
+        double compensate_temperature(uint32_t temperature_uncompensated);
+        double compensate_pressure(uint32_t pressure_uncompensated);
+        void get_calib_data();
+
+    private:
+        int handle = 0;
+        const static bool use_hyps = true;
+        CalibData calib_data;
+};
+
+#else // REALI2C
+
 #include <tuple>
 #include <string>
 #include <cstdlib>
@@ -158,5 +220,7 @@ class BMP384
         const static bool use_hyps = true;
         CalibData calib_data;
 };
+
+#endif
 
 #endif
