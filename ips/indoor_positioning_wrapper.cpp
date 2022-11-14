07b0b697ab955e1871154135893f4d46535171b5
Colin Broderick <colin.broderick@greeve.co.uk>
2022-11-14 14:40:47 +0000

    IPS refactored to namespace form

--- ips/indoor_positioning_wrapper.cpp
+++ ips/indoor_positioning_wrapper.cpp
@@ -8,96 +8,152 @@
 #include "corner_detector.hpp"
 #include "logger.hpp"
 #include "vector3.hpp"
-// #include "arwain.hpp"
+#include "exceptions.hpp"
+#include "arwain_thread.hpp"
+#include "arwain.hpp"
 
-/** \brief Indoor positioning system for recognising and snapping to stairs, floors, etc.
- * Runs as thread.
- */
-void indoor_positioning()
+namespace IndoorPositioningSystem
 {
-    return;
-    
-    // if (!arwain::config.use_ips)
-    // {
-    //     return;
-    // }
+    namespace // private
+    {
+        ArwainThread job_thread;
+        arwain::OperatingMode mode = arwain::OperatingMode::AutoCalibration;
 
-    // arwain::CornerDetector corner_detector{11, 115.0, 0.20}; // 11 * 0.2 means a window of 11 points separated by at least 20 cm each, so about 2 m total.
-    // arwain::FloorTracker floor_tracker{5, 0.10, 0.20}; // UK stairs are gradient approx. 0.9.
+        arwain::Logger corner_log;
+        arwain::Logger tracked_floor_log;
 
-    // while (arwain::system_mode != arwain::OperatingMode::Terminate)
-    // {
-    //     switch (arwain::system_mode)
-    //     {
-    //         case arwain::OperatingMode::Inference:
-    //         {
-    //             arwain::Logger corner_log;
-    //             arwain::Logger tracked_floor_log;
-    //             corner_log.open(arwain::folder_date_string + "/corner_log.txt");
-    //             corner_log << "time x y z\n";
-    //             tracked_floor_log.open(arwain::folder_date_string + "/tracked_floors.txt");
-    //             tracked_floor_log << "time x, y, z\n" ;
+        arwain::CornerDetector corner_detector{11, 115.0, 0.20}; // 11 * 0.2 means a window of 11 points separated by at least 20 cm each, so about 2 m total.
+        arwain::FloorTracker floor_tracker{5, 0.10, 0.20}; // UK stairs are gradient approx. 0.9.
 
-    //             auto time = std::chrono::high_resolution_clock::now();
-    //             std::chrono::milliseconds interval{arwain::Intervals::IPS_INTERVAL};
+        void core_setup()
+        {
+        }
 
-    //             while (arwain::system_mode == arwain::OperatingMode::Inference)
-    //             {
+        void setup_inference()
+        {
+            corner_log.open(arwain::folder_date_string + "/corner_log.txt");
+            corner_log << "time x y z\n";
+            tracked_floor_log.open(arwain::folder_date_string + "/tracked_floors.txt");
+            tracked_floor_log << "time x, y, z\n" ;
+        }
 
-    //                 // Wait until next tick.
-    //                 time = time + interval;
-    //                 std::this_thread::sleep_until(time);
+        void cleanup_inference()
+        {
+            corner_log.close();
+            tracked_floor_log.close();
+        }
 
-    //                 Vector3 new_position;
-    //                 new_position = arwain::Buffers::POSITION_BUFFER.back();
+        void run_inference()
+        {
+            setup_inference();
 
-    //                 if (corner_detector.update(new_position))
-    //                 {
-    //                     corner_log << time.time_since_epoch().count() << " "
-    //                             << corner_detector.detection_location.x << " "
-    //                             << corner_detector.detection_location.y << " "
-    //                             << corner_detector.detection_location.z << "\n";
-    //                 }
+            auto time = std::chrono::high_resolution_clock::now();
+            std::chrono::milliseconds interval{arwain::Intervals::IPS_INTERVAL};
 
-    //                 floor_tracker.update(new_position);
-    //                 // std::cout << time.time_since_epoch().count() << " "
-    //                 //           << floor_tracker.tracked_position.x << " "
-    //                 //           << floor_tracker.tracked_position.y << " "
-    //                 //           << floor_tracker.tracked_position.z << "\n";
-    //             }
-    //             break;
-    //         }
-    //         default:
-    //         {
-    //             sleep_ms(10);
-    //             break;
-    //         }
-    //     }
-    // }
+            while (mode == arwain::OperatingMode::Inference)
+            {
+                Vector3 new_position;
+                new_position = arwain::Buffers::POSITION_BUFFER.back();
+
+                if (corner_detector.update(new_position))
+                {
+                    corner_log << time.time_since_epoch().count() << " "
+                               << corner_detector.detection_location.x << " "
+                               << corner_detector.detection_location.y << " "
+                               << corner_detector.detection_location.z << "\n";
+                }
+
+                floor_tracker.update(new_position);
+
+                // Wait until next tick.
+                time = time + interval;
+                std::this_thread::sleep_until(time);
+            }
+
+            cleanup_inference();
+        }
+
+        void run_idle()
+        {
+            sleep_ms(10);
+        }
+
+        void run()
+        {
+            while (mode != arwain::OperatingMode::Terminate)
+            {
+                switch (mode)
+                {
+                case arwain::OperatingMode::Inference:
+                    run_inference();
+                    break;
+                
+                default:
+                    run_idle();
+                    break;
+                }
+            }
+        }
+    }
+
+    // Public
+
+    bool init()
+    {
+        if (!arwain::config.use_ips)
+        {
+            return false;
+        }
+        core_setup();
+        job_thread = ArwainThread{IndoorPositioningSystem::run, "arwain_ips_th"};
+        return true;
+    }
+
+    bool shutdown()
+    {
+        mode = arwain::OperatingMode::Terminate;
+        return true;
+    }
+
+    void join()
+    {
+        job_thread.join();
+    }
+
+    std::tuple<bool, std::string> set_mode(arwain::OperatingMode new_mode)
+    {
+        mode = new_mode;
+        return {true, "success"};
+    }
+
+    arwain::OperatingMode get_mode()
+    {
+        return mode;
+    }
 }
 
-void arwain::IndoorPositioningWrapper::update(const double &time, const double &x, const double &y, const double &z)
+void IndoorPositioningSystem::IndoorPositioningWrapper::update(const double &time, const double &x, const double &y, const double &z)
 {
     // TODO Pass the parameters to the inner IPS object and put results in
     // m_x, m_y, m_z.
 }
 
-Vector3 arwain::IndoorPositioningWrapper::getPosition()
+Vector3 IndoorPositioningSystem::IndoorPositioningWrapper::getPosition()
 {
     return {m_x, m_y, m_z};
 }
 
-double arwain::IndoorPositioningWrapper::getX()
+double IndoorPositioningSystem::IndoorPositioningWrapper::getX()
 {
     return m_x;
 }
 
-double arwain::IndoorPositioningWrapper::getY()
+double IndoorPositioningSystem::IndoorPositioningWrapper::getY()
 {
     return m_y;
 }
 
-double arwain::IndoorPositioningWrapper::getZ()
+double IndoorPositioningSystem::IndoorPositioningWrapper::getZ()
 {
     return m_z;
 }
