83be92d01d63d9ade36d5d7009cfc0bf3069111f
Colin Broderick <colin.broderick@greeve.co.uk>
2022-11-14 19:11:43 +0000

    Refactored experimental UUBLA functionality into namespace form

--- arwain_lib/uwb_reader.hpp
+++ arwain_lib/uwb_reader.hpp
@@ -1,6 +1,13 @@
 #ifndef _ARWAIN_UWB_READER_HPP
 #define _ARWAIN_UWB_READER_HPP
 
-void uwb_reader(const std::string& port, const int baudrate);
+namespace UublaWrapper
+{
+    bool init();
+    void join();
+    bool shutdown();
+    std::tuple<bool, std::string> set_mode(arwain::OperatingMode new_mode);
+    double get_distance(const int position);
+}
 
 #endif
