e52e06b0269f0fc355ae209ddbd51b058b932301
Cobain Attwood <cobain.attwood@greeve.co.uk>
2022-11-16 14:56:49 +0000

    more unit tests added

--- unit_tests/imu_reader_tests.cpp
+++ unit_tests/imu_reader_tests.cpp
@@ -2,194 +2,37 @@
 
 #include "imu_reader.hpp"
 
-void shutdown_thread()
-{
-    std::this_thread::sleep_for(std::chrono::seconds{1});
-
-    ImuProcessing::shutdown();
-}
-
-TEST(IMU_Reader, Set_Mode_Terminate)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::Terminate);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_Inference)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::Inference);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_Auto_Calabration)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::AutoCalibration);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_Self_Test)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::SelfTest);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_GyroscopeCalibration)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::GyroscopeCalibration);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_MagnetometerCalibration)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::MagnetometerCalibration);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_AccelerometerCalibration)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::AccelerometerCalibration);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_TestSerial)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::TestSerial);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_TestStanceDetector)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::TestStanceDetector);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_DataCollection)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::DataCollection);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-void example_callback()
-{
-    // do something
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_Terminate)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::Terminate, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_Inference)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::Inference, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_AutoCalibration)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::AutoCalibration, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_SelfTest)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::SelfTest, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_GyroscopeCalibration)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::GyroscopeCalibration, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_MagnetometerCalibration)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::MagnetometerCalibration, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_AccelerometerCalibration)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::AccelerometerCalibration, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_TestSerial)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::TestSerial, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_TestStanceDetector)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::TestStanceDetector, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Set_Mode_With_Func_DataCollection)
-{
-    std::tuple<bool, std::string> result;
-    result = ImuProcessing::set_mode(arwain::OperatingMode::DataCollection, example_callback);
-    EXPECT_TRUE(std::get<0>(result));
-}
-
-TEST(IMU_Reader, Shutdown)
-{
-    EXPECT_TRUE(ImuProcessing::shutdown());
-}
-
 TEST(IMU_Reader, Join)
 {
-    ImuProcessing::init();
-    std::thread thread(shutdown_thread);
-    thread.join();
-    EXPECT_NO_THROW(ImuProcessing::join());
+    ImuProcessing reader;
+    arwain::system_mode = arwain::OperatingMode::Terminate;
+    EXPECT_NO_THROW(reader.join());
 }
 
 TEST(IMU_Reader, Init_success)
 {
-    arwain::config.no_imu = false;
-    EXPECT_TRUE(ImuProcessing::init());
-    ImuProcessing::shutdown();
-    ImuProcessing::join();
+   /* arwain::config.no_imu = false;
+    ImuProcessing reader;
+    EXPECT_TRUE(reader.init());
+    arwain::system_mode = arwain::OperatingMode::Terminate;
+    reader.join();
+    */
+    FAIL();
 }
 
 TEST(IMU_Reader, Init_failure)
 {
+    /*
     arwain::config.no_imu = true;
-    EXPECT_FALSE(ImuProcessing::init());
+    ImuProcessing reader;
+    EXPECT_FALSE(reader.init());
+    arwain::system_mode = arwain::OperatingMode::Terminate;
+    reader.join();
+    */
+   FAIL();
 }
 
-TEST(IMU_Reader, Get_mode)
+TEST(IMU_Reader, Set_Post_Gyro_Calibration_Callback)
 {
-    arwain::OperatingMode mode;
-    arwain::OperatingMode original_mode = arwain::OperatingMode::AutoCalibration;
-    mode = ImuProcessing::get_mode();
-    bool result = false;
-    if (original_mode == mode)
-    {
-        result = true;
-        EXPECT_TRUE(result);
-    }
+   FAIL();
 }
--- unit_tests/indoor_positioning_wrapper_tests.cpp
+++ unit_tests/indoor_positioning_wrapper_tests.cpp
@@ -1,42 +1,105 @@
 #include <gtest/gtest.h>
 
 #include "indoor_positioning_wrapper.hpp"
-#include "floor_tracker.hpp"
-#include "corner_detector.hpp"
-#include "logger.hpp"
-#include "vector3.hpp"
+//#include "floor_tracker.hpp"
+//#include "corner_detector.hpp"
+//#include "logger.hpp"
+//#include "vector3.hpp"
 #include "arwain.hpp"
 
-TEST(Indoor_Positioning, Update)
+//public functions
+TEST(Indoor_Positioning_Public, Init_Success)
 {
+    /*IndoorPositioningSystem pos_system;
+    arwain::config.use_ips = true;
+    EXPECT_TRUE(pos_system.init());
+    arwain::system_mode = arwain::OperatingMode::Terminate;
+    pos_system.join();*/
+
     FAIL();
 }
 
-TEST(Indoor_Positioning, getPosition)
+TEST(Indoor_Positioning_Public, Init_Failure)
 {
-    arwain::IndoorPositioningWrapper wrapper;
+    /*IndoorPositioningSystem pos_system;
+    arwain::config.use_ips = false;
+    EXPECT_FALSE(pos_system.init());
+    arwain::system_mode = arwain::OperatingMode::Terminate;
+    pos_system.join();*/
+
+    FAIL();
+}
+
+TEST(Indoor_Positioning_Public, Join)
+{
+    IndoorPositioningSystem pos_system;
+    //pos_system.init();
+    arwain::system_mode = arwain::OperatingMode::Terminate;
+    EXPECT_NO_THROW(pos_system.join());
+}
+
+//private functions
+TEST(Indoor_Positioning_Private, Core_Setup)
+{
+    FAIL(); // function not implemented yet!
+}
+
+TEST(Indoor_Positioning_Private, Setup_Inference)
+{
+    FAIL(); // function not implemented yet!
+}
+
+TEST(Indoor_Positioning_Private, Cleanup_Inference)
+{
+    FAIL(); // function not implemented yet!
+}
+
+TEST(Indoor_Positioning_Private, Run_Idle)
+{
+    FAIL(); // function not implemented yet!
+}
+
+TEST(Indoor_Positioning_Private, Run_Inference)
+{
+    FAIL(); // function not implemented yet!
+}
+
+TEST(Indoor_Positioning_Private, Run)
+{
+    FAIL(); // function not implemented yet!
+}
+
+//public wrapper functions
+TEST(Indoor_Positioning_Wrapper, Update)
+{
+    FAIL(); // function not implemented yet!
+}
+
+TEST(Indoor_Positioning_Wrapper, getPosition)
+{
+    IndoorPositioningSystem::IndoorPositioningWrapper wrapper;
     Vector3 pos{1.0, 1.0, 1.0};
 
     EXPECT_EQ(pos, wrapper.getPosition());
 }
 
-TEST(Indoor_Positioning, getX)
+TEST(Indoor_Positioning_Wrapper, getX)
 {
-    arwain::IndoorPositioningWrapper wrapper;
+    IndoorPositioningSystem::IndoorPositioningWrapper wrapper;
     double x = 1.0;
     EXPECT_EQ(x, wrapper.getX());
 }
 
-TEST(Indoor_Positioning, getY)
+TEST(Indoor_Positioning_Wrapper, getY)
 {
-    arwain::IndoorPositioningWrapper wrapper;
+    IndoorPositioningSystem::IndoorPositioningWrapper wrapper;
     double y = 1.0;
     EXPECT_EQ(y, wrapper.getY());
 }
 
-TEST(Indoor_Positioning, getZ)
+TEST(Indoor_Positioning_Wrapper, getZ)
 {
-    arwain::IndoorPositioningWrapper wrapper;
+    IndoorPositioningSystem::IndoorPositioningWrapper wrapper;
     double z = 1.0;
     EXPECT_EQ(z, wrapper.getZ());
 }
