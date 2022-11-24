4d4d2df9d483e54ae039f87ec3c83b7f2d04d043
Colin Broderick <colin.broderick@greeve.co.uk>
2022-11-24 15:12:35 +0000

    Organized and documented calibration code

--- arwain_lib/include/calibration.hpp
+++ arwain_lib/include/calibration.hpp
@@ -19,6 +19,7 @@ class MagnetometerCalibrator
         int sphere_region(const double x, const double y, const double z) const;
 
     private:
+        constexpr static double pi = 3.14159265358979323846;
         int feed_count = 0;
         int sphere_coverage_quality = 0;
         std::array<int, 100> region_sample_count = {0};
@@ -38,78 +39,11 @@ class AccelerometerCalibrator
         std::vector<Vector3> samplings;
 
     public:
-        AccelerometerCalibrator()
-        {
-
-        }
-
-        bool is_converged()
-        {
-            return converged;
-        }
-
-        Vector3 get_params()
-        {
-            return {kfx.est, kfy.est, kfz.est};
-        }
-
-        void next_sampling()
-        {
-            converged = false;
-            samplings.push_back({kfx.est, kfy.est, kfz.est});
-            kfx = KalmanFilter1D{9.81, 1.0};
-            kfy = KalmanFilter1D{9.81, 1.0};
-            kfz = KalmanFilter1D{9.81, 1.0};
-        }
-
-        bool feed(const Vector3& reading)
-        {
-            kfx.update(reading.x, 0.1);
-            kfy.update(reading.y, 0.1);
-            kfz.update(reading.z, 0.1);
-
-            if (!kfx.converged || !kfy.converged || !kfz.converged)
-            {
-                converged = false;
-            }
-            else
-            {
-                converged = true;
-            }
-
-            return converged;
-        }
-
-        std::tuple<Vector3, Vector3> deduce_calib_params()
-        {
-            double x_min = 1e6;
-            double x_max = -1e6;
-            double y_min = 1e6;
-            double y_max = -1e6;
-            double z_min = 1e6;
-            double z_max = -1e6;
-            
-            for (const Vector3& vec : samplings)
-            {
-                x_min = vec.x < x_min ? vec.x : x_min;
-                x_max = vec.x > x_max ? vec.x : x_max;
-                y_min = vec.y < y_min ? vec.y : y_min;
-                y_max = vec.y > y_max ? vec.y : y_max;
-                z_min = vec.z < z_min ? vec.z : z_min;
-                z_max = vec.z > z_max ? vec.z : z_max;
-            }
-            Vector3 bias_ = {(x_min + x_max) / 2.0, (y_min + y_max) / 2.0, (z_min + z_max) / 2.0};
-
-            // Compute scale correction factors.
-            Vector3 delta = {(x_max - x_min) / 2.0, (y_max - y_min) / 2.0, (z_max - z_min) / 2.0};
-            double average_delta = (delta.x + delta.y + delta.z)/3.0;
-            double scale_x = average_delta / delta.x;
-            double scale_y = average_delta / delta.y;
-            double scale_z = average_delta / delta.z;
-            Vector3 scale_ = {scale_x, scale_y, scale_z};
-
-            return {bias_, scale_};
-        }
+        bool is_converged();
+        Vector3 get_params();
+        void next_sampling();
+        bool feed(const Vector3& reading);
+        std::tuple<Vector3, Vector3> deduce_calib_params();
 };
 
 /** \brief Calibration tool for a 3-axis digital gyroscope. */
@@ -122,37 +56,9 @@ class GyroscopeCalibrator
         KalmanFilter1D kfz{0, 1};
 
     public:
-        GyroscopeCalibrator()
-        {
-        }
-
-        bool is_converged()
-        {
-            return converged;
-        }
-
-        Vector3 get_params()
-        {
-            return {kfx.est, kfy.est, kfz.est};
-        }
-
-        bool feed(const Vector3& reading)
-        {
-            kfx.update(reading.x, 0.02);
-            kfy.update(reading.y, 0.02);
-            kfz.update(reading.z, 0.02);
-
-            if (!kfx.converged || !kfy.converged || !kfz.converged)
-            {
-                converged = false;
-            }
-            else
-            {
-                converged = true;
-            }
-
-            return converged;
-        }
+        bool is_converged();
+        Vector3 get_params();
+        bool feed(const Vector3& reading);
 };
 
 #endif
