1c81ca53139ddaaac9ad113c1ff68a99ce935da3
Cobain_Attwood <cobain.attwood@greeve.co.uk>
2023-01-03 10:18:47 +0000

    small changes

---
+++ unit_tests/command_line_tests.cpp
@@ -0,0 +1,97 @@
+#include <gtest/gtest.h>
+#include "command_line.hpp"
+
+TEST(Command_Line, run)
+{
+    FAIL();
+}
+
+TEST(Command_Line, s2i)
+{
+    FAIL();
+}
+
+TEST(Command_Line, switch_to_exit_mode)
+{
+    FAIL();
+}
+
+TEST(Command_Line, report_current_mode)
+{
+    FAIL();
+}
+
+TEST(Command_Line, switch_to_idle_autocal_mode)
+{
+    FAIL();
+}
+
+TEST(Command_Line, switch_to_inference_mode)
+{
+    FAIL();
+}
+
+TEST(Command_Line, core_setup)
+{
+    FAIL();
+}
+
+TEST(Command_Line, force_switch_to_idle_autocal_mode)
+{
+    FAIL();
+}
+
+TEST(Command_Line, switch_to_gyro_calib_mode)
+{
+    FAIL();
+}
+
+TEST(Command_Line, fail_to_switch_to)
+{
+    FAIL();
+}
+
+TEST(Command_Line, switch_to_mag_calib_mode)
+{
+    FAIL();
+}
+
+TEST(Command_Line, switch_to_accel_calib_mode)
+{
+    FAIL();
+}
+
+TEST(Command_Line, switch_to_data_collection_mode)
+{
+    FAIL();
+}
+
+TEST(Command_Line, set_folder_name)
+{
+    FAIL();
+}
+
+TEST(Command_Line, parse_cli_input)
+{
+    FAIL();
+}
+
+TEST(Command_Line, set_velocity_inference_pointer)
+{
+    FAIL();
+}
+
+TEST(Command_Line, constructor)
+{
+    FAIL();
+}
+
+TEST(Command_Line, init)
+{
+    FAIL();
+}
+
+TEST(Command_Line, join)
+{
+    FAIL();
+}
