4c01a07ae9b71084eaab29146aac657a52cffcf6
Colin Broderick <colin.broderick@greeve.co.uk>
2023-02-28 08:28:07 +0000

    Made more of PositionVelocityInference testable, and cleanup_inference() test now passes.

--- unit_tests/velocity_predication_tests.cpp
+++ unit_tests/velocity_predication_tests.cpp
@@ -63,17 +63,42 @@ TEST(Velocity_Prediction, Run)
    FAIL();
 }
 
+/** \brief After the cleanup_inference() call, position and velocity
+ * should both be zero-vector regardless of what they were before.
+ * Log files should both be closed.
+ */
 TEST(Velocity_Prediction, Cleanup_Inference)
 {
-    FAIL();
+    PositionVelocityInference inferrer;
+    inferrer.position = {1, 1, 1};
+    inferrer.velocity = {1, 1, 1};
+    EXPECT_EQ(inferrer.position, (Vector3{1, 1, 1}));
+    EXPECT_EQ(inferrer.velocity, (Vector3{1, 1, 1}));
+    inferrer.position_file.open("/dev/null");
+    inferrer.velocity_file.open("/dev/null");
+    EXPECT_TRUE(inferrer.position_file.is_open());
+    EXPECT_TRUE(inferrer.velocity_file.is_open());
+
+    inferrer.cleanup_inference();
+
+    EXPECT_EQ(inferrer.position, (Vector3{0, 0, 0}));
+    EXPECT_EQ(inferrer.velocity, (Vector3{0, 0, 0}));
+    EXPECT_FALSE(inferrer.position_file.is_open());
+    EXPECT_FALSE(inferrer.velocity_file.is_open());
+
+    arwain::system_mode = arwain::OperatingMode::Terminate;
+    inferrer.join();
 }
 
 #if USE_NCS2
+/** \brief This function just calls a Python script then returns.
+ * Nothing else is testable at this stage. In fact it will probably
+ * fail to find the script, but we can't test that effectively.
+ * 
+ * TODO Improve testability of py_inference funciton.
+ */
 TEST(Velocity_Prediction, Py_Inference)
 {
-    // TODO Improve testability of py_inference funciton.
-    // This function just calls a Python script then returns. Nothing else is testable at this stage.
-    // In fact it will probably fail to find the script, but we can't test that effectively.
     PositionVelocityInference inferrer;
     inferrer.py_inference();
     arwain::system_mode = arwain::OperatingMode::Terminate;
