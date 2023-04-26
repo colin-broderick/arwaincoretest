36fb111480f7ba27da8d2cc12c67a7292ec414a3
Colin Broderick <colin.broderick@greeve.co.uk>
2023-04-26 07:15:20 +0000

    Expanded doxygen on EventManager system

--- arwain_lib/include/event_manager.hpp
+++ arwain_lib/include/event_manager.hpp
@@ -7,17 +7,26 @@
 
 #include "arwain_modes.hpp"
 
+/** \brief A synchronous event manager allowing an unlimited number of callback registrations.
+ *
+ * Callbacks are registered using add_callback(). Callbacks are deregistered using remove_callback().
+ * Registered callbacks many be called using invoke(). Currently, only single-argument callbacks
+ * are supported. Changes are planned to support asynchronous callbacks.
+ * 
+ * \tparam FuncSig: The signature of the expected callback functions.
+ * \tparam InvokeArg: The type of the argument to be passed to callbacks.
+ */
 template <class FuncSig, class InvokeArg>
 class Event
 {
-    private:
-        std::mutex lock;
-        uint64_t current_key = 0;
-        std::map<uint64_t, std::function<FuncSig>> callbacks;
-
     public:
 
-        /** \brief Register a new callback. */
+        /** \brief Register a new callback.
+         * \param func A function pointer to the function to be called when the event
+         * is triggered.
+         * \return A callback registration key which is unique to this event. The key
+         * should be  used to deregister the callback.
+         */
         uint64_t add_callback(std::function<FuncSig> func)
         {
             std::lock_guard<std::mutex> lock_guard{lock};
@@ -26,12 +35,17 @@ class Event
             return current_key;
         }
         
+        /** \brief Get the count of callbacks registered to this event. 
+         * \return Count of registered callbacks.
+         */
         std::size_t size()
         {
             return callbacks.size();
         }
 
-        /** \brief Call all registered callbacks with the provided argument. */
+        /** \brief Call all registered callbacks with the provided argument.
+         * \param arg The value of the argument to pass to the callbacks.
+         */
         void invoke(InvokeArg arg)
         {
             std::lock_guard<std::mutex> lock_guard{lock};
@@ -41,8 +55,8 @@ class Event
             }
         }
 
-        /** \brief Removes a callback from the action if the provided key is known. 
-         * \param key A deregistration key which will have been provided when the callback was registered.
+        /** \brief Removes a callback from the event if the provided key is known.
+         * \param key A deregistration key which was provided when the callback was registered.
          */
         void remove_callback(uint64_t key)
         {
@@ -50,29 +64,39 @@ class Event
             callbacks.erase(key);
         }
 
-        /** \brief Erase all callbacks from the action. */
+        /** \brief Erase all callbacks from the event. */
         void clear()
         {
             std::lock_guard<std::mutex> lock_guard{lock};
             callbacks.clear();
         }
+
+    private:
+        /** \brief All accessors lock this mutex to provide memory safety. */
+        std::mutex lock;
+
+        /** \brief Increments on each callback registration so that registartion 
+         * keys are unique.
+         */
+        uint64_t current_key = 0;
+
+        /** \brief A container for all registered callbacks. */
+        std::map<uint64_t, std::function<FuncSig>> callbacks;
 };
 
 namespace EventManager
 {
+    /** \brief Subscribe to this event to trigger a callback when Arwain system mode changes. */
     inline Event<void(arwain::OperatingMode), arwain::OperatingMode> switch_mode_event;
 }
 
+/** \brief This class exists to provide a means to track system mode in the cases where no
+ * ArwainJobs are running. It has no particular utility outside of the Arwain codebase.
+ * The class provides a simple callback which, registered to the switch_mode_event, which
+ * updates internal mode state when the event is triggered. Useful for testing.
+ */
 class StandAloneModeRegistrar
 {
-    TESTABLE:
-        uint64_t deregistration_key = 0;
-        arwain::OperatingMode mode = arwain::OperatingMode::Idle;
-        void callback(arwain::OperatingMode new_mode)
-        {
-            mode = new_mode;
-        }
-
     public:
         StandAloneModeRegistrar()
         {
@@ -87,6 +111,20 @@ class StandAloneModeRegistrar
         arwain::OperatingMode get_mode() const
         {
             return mode;
+        }
+
+    TESTABLE:
+        /** \brief Key used to deregister the callback when this class's destructor is called. */
+        uint64_t deregistration_key = 0;
+
+        /** \brief Current Arwain system mode, updated when the callback is called. */
+        arwain::OperatingMode mode = arwain::OperatingMode::Idle;
+
+    TESTABLE:
+        /** \brief Called when the switch_mode_event is triggered. Sets the internal system mode. */
+        void callback(arwain::OperatingMode new_mode)
+        {
+            mode = new_mode;
         }
 };
 
