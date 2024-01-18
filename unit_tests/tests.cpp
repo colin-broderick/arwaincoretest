46ccb454a05f092ae91f415b254619c3930e5a85
Colin Broderick <colin.broderick@greeve.co.uk>
2024-01-18 10:54:12 +0000

    Fixed misuse of gtest which was causing tests to hang

--- unit_tests/tests.cpp
+++ unit_tests/tests.cpp
@@ -2,13 +2,9 @@
 
 // These pointers retained as some legacy tests still use them.
 // New tests should print to stringstream or similar instead.
-std::streambuf* original_cout_buffer;
-std::streambuf* original_cerr_buffer;
 
 int main(int argc, char* argv[])
 {
-    original_cout_buffer = std::cout.rdbuf(nullptr);
-    original_cerr_buffer = std::cerr.rdbuf(nullptr);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
