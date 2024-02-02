fbd49c0c1a7272a0a8e7d3831080d7bc3b2312a0
Colin Broderick <colin.broderick@greeve.co.uk>
2024-02-02 11:30:08 +0000

    Job interface members now public

--- arwain_lib/include/arwain/job_interface.hpp
+++ arwain_lib/include/arwain/job_interface.hpp
@@ -7,16 +7,15 @@
 
 class IArwainJob
 {
-  public:
+    public:
         virtual bool init() = 0;
         virtual bool join() = 0;
-  protected:
         virtual void run() = 0;
 };
 
 class IArwainJobSpec
 {
-    protected:
+    public:
         virtual void core_setup() = 0;
         virtual void run_inference() = 0;
         virtual void run_idle() = 0;
