29ba954821f1c8fd336d2ba70603eb1ab5b5e6b7
Colin Broderick <colin.broderick@greeve.co.uk>
2024-02-07 20:12:59 +0000

    Added NumCpp dependecy

--- arwain_libs.cmake
+++ arwain_libs.cmake
@@ -1,3 +1,5 @@
+include(FetchContent)
+
 function(provide_library url as_name tag)
     FetchContent_Declare(
         ${as_name}
@@ -9,6 +11,31 @@ function(provide_library url as_name tag)
     include_directories(${${as_name}_SOURCE_DIR}/include)
 endfunction()
 
+FetchContent_Declare(
+    simple-websocket-server
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/_deps/sws/simple-websocket-server
+    GIT_REPOSITORY https://gitlab.com/eidheim/Simple-WebSocket-Server
+    GIT_PROGRESS TRUE
+    GIT_SHALLOW TRUE
+    GIT_TAG v2.0.2)
+FetchContent_MakeAvailable(simple-websocket-server)
+include_directories(${CMAKE_BINARY_DIR}/_deps/sws)
+
+if(BUILT_WITH_YOCTO)
+else()
+    FetchContent_Declare(
+        tensorflow
+        GIT_REPOSITORY https://github.com/tensorflow/tensorflow.git
+        GIT_PROGRESS TRUE
+        GIT_SHALLOW TRUE
+        GIT_TAG v2.10.0
+        SOURCE_SUBDIR tensorflow/lite)
+    FetchContent_MakeAvailable(tensorflow)
+    include_directories(./build/_deps/tensorflow-src)
+    include_directories(./build/flatbuffers/include)
+endif()
+
+provide_library("https://github.com/dpilger26/NumCpp.git"           NumCpp              Version_2.12.1)
 provide_library("https://bitbucket.org/arwain/arwain_math"          arwain_math         master)
 provide_library("https://bitbucket.org/arwain/arwain_config"        arwain_config       master)
 provide_library("https://bitbucket.org/arwain/bmp384"               bmp384              master)
