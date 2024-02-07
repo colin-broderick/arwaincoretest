41238894ee315394bad514e89c49ffd422a3cc02
Colin Broderick <colin.broderick@greeve.co.uk>
2024-02-07 18:59:17 +0000

    Further simplified/abstracted dependency management

--- arwain_libs.cmake
+++ arwain_libs.cmake
@@ -1,116 +1,26 @@
-FetchContent_Declare(
-    arwain_config
-    GIT_REPOSITORY https://bitbucket.org/arwain/arwain_config
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(arwain_config)
-include_directories(${arwain_config_SOURCE_DIR}/include)
+set(FETCHCONTENT_QUIET FALSE)
 
-FetchContent_Declare(
-    bmp384
-    GIT_REPOSITORY https://bitbucket.org/arwain/bmp384
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(bmp384)
-include_directories(${bmp384_SOURCE_DIR}/include)
+function(provide_library url as_name tag)
+    FetchContent_Declare(
+        ${as_name}
+        GIT_REPOSITORY ${url}
+        GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
+        GIT_TAG ${tag})
+    FetchContent_MakeAvailable(${as_name})
+    include_directories(${${as_name}_SOURCE_DIR}/include)
+endfunction()
 
-FetchContent_Declare(
-    logger
-    GIT_REPOSITORY https://bitbucket.org/arwain/logger
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(logger)
-include_directories(${logger_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    timers
-    GIT_REPOSITORY https://bitbucket.org/arwain/timers
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(timers)
-include_directories(${timers_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    input_parser
-    GIT_REPOSITORY https://bitbucket.org/arwain/input_parser
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(input_parser)
-include_directories(${input_parser_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    arwain_rfm95w
-    GIT_REPOSITORY https://bitbucket.org/arwain/arwain_rfm95w
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(arwain_rfm95w)
-include_directories(${arwain_rfm95w_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    iim42652
-    GIT_REPOSITORY https://bitbucket.org/arwain/iim42652
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(iim42652)
-include_directories(${iim42652_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    arwain_event
-    GIT_REPOSITORY https://bitbucket.org/arwain/arwain_event
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(arwain_event)
-include_directories(${arwain_event_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    uublacpp
-    GIT_REPOSITORY https://bitbucket.org/arwain/uublacpp
-    GIT_SHALLOW TRUE
-    GIT_TAG temp17
-)
-FetchContent_MakeAvailable(uublacpp)
-include_directories(${uublacpp_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    lis3mdl
-    GIT_REPOSITORY https://bitbucket.org/arwain/lis3mdl
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(lis3mdl)
-include_directories(${lis3mdl_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    arwain_orientation
-    GIT_REPOSITORY https://bitbucket.org/arwain/arwain_orientation
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(arwain_orientation)
-include_directories(${arwain_orientation_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    arwain_icp
-    GIT_REPOSITORY https://bitbucket.org/arwain/arwain_icp
-    GIT_SHALLOW TRUE
-    GIT_TAG modern
-)
-FetchContent_MakeAvailable(arwain_icp)
-include_directories(${arwain_icp_SOURCE_DIR}/include)
-
-FetchContent_Declare(
-    arwain_websocket
-    GIT_REPOSITORY https://bitbucket.org/arwain/arwain_websocket
-    GIT_SHALLOW TRUE
-    GIT_TAG master
-)
-FetchContent_MakeAvailable(arwain_websocket)
-include_directories(${arwain_websocket_SOURCE_DIR}/include)
+provide_library("https://bitbucket.org/arwain/arwain_config"        arwain_config       master)
+provide_library("https://bitbucket.org/arwain/bmp384"               bmp384              master)
+provide_library("https://bitbucket.org/arwain/logger"               logger              master)
+provide_library("https://bitbucket.org/arwain/timers"               timers              master)
+provide_library("https://bitbucket.org/arwain/input_parser"         input_parser        master)
+provide_library("https://bitbucket.org/arwain/arwain_rfm95w"        arwain_rfm95w       master)
+provide_library("https://bitbucket.org/arwain/iim42652"             iim42652            master)
+provide_library("https://bitbucket.org/arwain/arwain_event"         arwain_event        master)
+provide_library("https://bitbucket.org/arwain/uublacpp"             uublacpp            temp17)
+provide_library("https://bitbucket.org/arwain/lis3mdl"              lis3mdl             master)
+provide_library("https://bitbucket.org/arwain/arwain_orientation"   arwain_orientation  master)
+provide_library("https://bitbucket.org/arwain/arwain_icp"           arwain_icp          modern)
+provide_library("https://bitbucket.org/arwain/arwain_websocket"     arwain_websocket    master)
