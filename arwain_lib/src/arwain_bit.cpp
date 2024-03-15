8f0603844d5a136b9700e54b49f686ff94a30abe
Colin Broderick <colin.broderick@greeve.co.uk>
2024-03-15 11:22:01 +0000

    Merged in bit (pull request #35)
    Initial startup BIT implementation
    
    * Initial startup BIT implementation
    
    * Merged uwbtidy into bit

---
+++ arwain_lib/src/arwain_bit.cpp
@@ -0,0 +1,68 @@
+#include <arwain/input_parser.hpp>
+#include <arwain/devices/iim42652.hpp>
+#include <arwain/devices/rfm95w.hpp>
+#include <arwain/devices/lis3mdl.hpp>
+#include <arwain/devices/bmp384.hpp>
+
+#include "arwain/i2c_interface.hpp"
+#include "arwain/arwain.hpp"
+#include "arwain/arwain_bit.hpp"
+#include "arwain/configuration.hpp"
+
+ArwainBIT arwain_bit(int argc, char **argv)
+{
+    try
+    {
+        arwain::InputParser input{argc, argv};
+        arwain::config = arwain::Configuration{input};
+    }
+    catch (const std::exception &e)
+    {
+        return ArwainBIT::CONFIG_FAILED;
+    }
+
+    // Test IMUs.
+    IIM42652<LinuxSmbusI2CDevice> imu1{arwain::config.imu1_address, arwain::config.imu1_bus};
+    if (imu1.whoami() != IIM42652<LinuxSmbusI2CDevice>::chip_id)
+    {
+        return ArwainBIT::IMU1_FAILED;
+    }
+    IIM42652<LinuxSmbusI2CDevice> imu2{arwain::config.imu2_address, arwain::config.imu2_bus};
+    if (imu2.whoami() != IIM42652<LinuxSmbusI2CDevice>::chip_id)
+    {
+        return ArwainBIT::IMU2_FAILED;
+    }
+    IIM42652<LinuxSmbusI2CDevice> imu3{arwain::config.imu3_address, arwain::config.imu3_bus};
+    if (imu3.whoami() != IIM42652<LinuxSmbusI2CDevice>::chip_id)
+    {
+        return ArwainBIT::IMU3_FAILED;
+    }
+
+    // Test pressure
+    BMP384<LinuxSmbusI2CDevice> pressure_sensor{arwain::config.pressure_address, arwain::config.pressure_bus};
+    auto chip_id = pressure_sensor.whoami();
+    bool condition = (chip_id == BMP384<LinuxSmbusI2CDevice>::BMP384_chip_id) || (chip_id == BMP384<LinuxSmbusI2CDevice>::BMP390_chip_id);
+    if (!condition)
+    {
+        return ArwainBIT::PRESSURE_FAILED;
+    }
+
+    // Test magnetometer
+    LIS3MDL<LinuxSmbusI2CDevice> magnetometer{arwain::config.magn_address, arwain::config.magn_bus};
+    if (magnetometer.whoami() != LIS3MDL<LinuxSmbusI2CDevice>::chip_id)
+    {
+        return ArwainBIT::MAG_FAILED;
+    }
+
+    // Test LoRa
+    RFM95W<LinuxSpiDevice> lora{arwain::config.lora_address, AsReceiver::No};
+    if (lora.test_chip() != RFM95W<LinuxSpiDevice>::chip_id)
+    {
+        return ArwainBIT::LORA_FAILED;
+    }
+
+    // TODO Test UWB
+    // return ArwainBIT::UWB_FAILED;
+
+    return ArwainBIT::ALL_OK;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "arwain/arwain.hpp"
+#include "arwain/arwain_bit.hpp"
 
 /** \brief Program entry point.
  * \param argc Number of arguments.
  * \param argv List of arguments.
  */
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
+    auto bit_result = arwain_bit(argc, argv);
+    if (bit_result != ArwainBIT::ALL_OK)
+    {
+        return static_cast<int>(bit_result);
+    }
     return static_cast<int>(arwain_main(argc, argv));
 }
